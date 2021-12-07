96eff90109fbe9c5bc4ae8dd8abaa9922a5d0463
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-12-07 22:02:17 +0100

    Day 7: The Treachery of Whales - in haskell
    keep only smallest, do not stuff everyting into memory

--- 07-Whales.hs
+++ 07-Whales.hs
@@ -1,4 +1,4 @@
-module Whales where
+module Main where
 
 import Data.List (sortOn,nub)
 import Utils (atoiList)
@@ -20,10 +20,12 @@ incfuel :: FuelCalculator
 incfuel xs pos = map steptofuel $ steps xs pos
 
 solve :: FuelCalculator -> [CrabPosition] -> (CrabPosition, FuelCost)
-solve calculateFuel input = head $ sortOn snd positionsWithFuel
-  where
-    positionsWithFuel = map totalFuelOnPosition $ nub input
-    totalFuelOnPosition pos = (pos, sum $ calculateFuel input pos)
+solve calculateFuel input = foldl minTotalFuel (0, totalFuelOnPosition 0) $ nub input
+    where totalFuelOnPosition pos = sum $ calculateFuel input pos
+          minTotalFuel (p1, c1) pos
+           | c < c1    = (pos,c)
+           | otherwise = (p1, c1)
+           where c = totalFuelOnPosition pos
 
 main = do
   contents <- getContents
