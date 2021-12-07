7cbb0a22c28eb90ea91dd8a1bb945ac147c14d95
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-12-07 06:35:08 +0100

    Day 7: The Treachery of Whales - in javascript

--- 07-.js
+++ 07-.js
@@ -18,6 +18,10 @@ const sum = input => input.reduce((a,b)=>a+b)
 const subtract = (arr1, arr2) => zipWith(sub, arr1, arr2)
 const fill = (length,num) => Array(length).fill(num)
 
+const stepToFuel = n => n * (n + 1) / 2
+
+const incfuel = (arr1, arr2) => subtract(arr1, arr2).map(stepToFuel)
+
 function partOne(input) {
   const min = Math.min(...input)
   const max = Math.max(...input)
@@ -37,5 +41,18 @@ function partOne(input) {
 }
 
 function partTwo(lines) {
-  return 'todo'
+  const min = Math.min(...input)
+  const max = Math.max(...input)
+  const len = input.length
+  let hor = 0
+  let fuel = Number.MAX_SAFE_INTEGER
+
+  for (let i=min; i<=max; i+=1) {
+    const cost = sum(incfuel(input, fill(len, i)))
+    if (cost < fuel) {
+      fuel = cost
+      hor = i
+    }
+  }
+  return {hor, fuel}
 }
