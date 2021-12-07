a3330b79fde7f5f48a4782884eb96062104e7a31
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-12-07 07:44:15 +0100

    Day 3: Binary Diagnostics in haskell
    linting

--- 03-Binary-Diagnostics.hs
+++ 03-Binary-Diagnostics.hs
@@ -2,8 +2,8 @@ module BinaryDiagnostics where
 -- runhaskell 03-Binary-Diagnostics.hs <03-input.txt
 sp :: Num a => [Char] -> [a]
 sp [] = []
-sp ('1':xs) = [1] ++ sp xs
-sp ('0':xs) = [0] ++ sp xs
+sp ('1':xs) = 1 : sp xs
+sp ('0':xs) = 0 : sp xs
 sp (_:xs) = sp xs
 
 tally :: (Foldable t, Num c) => t [c] -> [c]
@@ -11,31 +11,31 @@ tally = foldl (zipWith (+)) $ replicate 12 0
 
 bintodec :: [Bool] -> Int
 bintodec xs = bintodec' 0 $ reverse xs
-  where 
+  where
     bintodec' n []  = 0
     bintodec' n (True:xs) = (2 ^ n) + bintodec' (n+1) xs
     bintodec' n (False:xs) =  bintodec' (n+1) xs
 
 filterLines :: (Int -> Int -> Bool) -> [[Int]] -> [Int]
-filterLines pred xss = solve xss 0 
-   where 
-     solve (xs:[]) n = xs
+filterLines pred xss = solve xss 0
+   where
+     solve [xs] n = xs
      solve xss n = solve filtered (n+1)
         where
-          numOnes = foldl (+) 0 $ map (\xs -> xs !! n) xss
+          numOnes = sum $ map (!! n) xss
           numZeroes = length xss - numOnes
-          filtered = filter (\xs -> (xs !! n) == (fromEnum (pred numZeroes numOnes))) xss
+          filtered = filter (\xs -> (xs !! n) == fromEnum (pred numZeroes numOnes)) xss
 
 
 partOne :: [[Int]] -> Int
-partOne xss = (bintodec gamma) * (bintodec epsilon)
-  where 
+partOne xss = bintodec gamma * bintodec epsilon
+  where
       half = div (length xss) 2
       gamma = map (half <) $ tally xss
       epsilon = map not gamma
 
 partTwo :: [[Int]] -> Int
-partTwo xss = (bintodec oxygen) * (bintodec co2)
+partTwo xss = bintodec oxygen * bintodec co2
   where
     oxygen = map (1==) $ filterLines (<=) xss
     co2 = map (1==) $ filterLines (>) xss
