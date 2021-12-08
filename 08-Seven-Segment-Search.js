1303b2670e6f166d034aac3abf44d748aa6d689e
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-12-08 18:12:55 +0100

    Day 8: Seven Segment Search - javascript
    cleanup

--- 08-Seven-Segment-Search.js
+++ 08-Seven-Segment-Search.js
@@ -4,11 +4,6 @@ const rl = readline.createInterface({ input: process.stdin })
 
 const lines = []
 
-function includes(str, substr) {
-  const diff = R.difference(substr.split(''), str.split('')) 
-  return diff.length === 0
-}
-
 rl.on("line", data => {
   const [signalPatterns, outputValues] = data.split(' | ').map(v=>v.split(' '))
 
@@ -29,34 +24,37 @@ function partOne(lines) {
   return sum
 }
 
+const includes = (str, substr) => R.difference(substr, str).length === 0
 const sortStr = str => str.split('').sort().join('')
+const toSortedArrays = str => str.split('').sort()
 
 function analyze(signalPatterns) {
+  const patterns = signalPatterns.map(toSortedArrays)
 
-    one = signalPatterns.find(s => s.length === 2)
-    four = signalPatterns.find(s => s.length === 4)
-    seven = signalPatterns.find(s => s.length === 3)
-    eight = signalPatterns.find(s => s.length === 7)
+  const one = patterns.find(s => s.length === 2)
+  const four = patterns.find(s => s.length === 4)
+  const seven = patterns.find(s => s.length === 3)
+  const eight = patterns.find(s => s.length === 7)
 
-    six = signalPatterns.find(s => s.length === 6 && (!s.includes(one.charAt(0)) || !s.includes(one.charAt(1))))
+  const three = patterns.find(s => s.length === 5 && includes(s, one))
 
-    three = signalPatterns.find(s => s.length === 5 && includes(s, one))
-    nine = signalPatterns.find(s => s.length === 6 && includes(s, three))
+  const six = patterns.find(s => s.length === 6 && (!s.includes(one[0]) || !s.includes(one[1])))
+  const nine = patterns.find(s => s.length === 6 && includes(s, three))
+  const zero = patterns.find(s => s.length === 6 && s !== six && s !== nine)
 
-    zero = signalPatterns.find(s => s.length === 6 && s !== six && s !== nine)
-    five = signalPatterns.find(s => s.length == 5 && includes(nine, s) && s !== three)
-    two = signalPatterns.find(s => s.length == 5 && s !== three && s !== five)
+  const five = patterns.find(s => s.length == 5 && includes(nine, s) && s !== three)
+  const two = patterns.find(s => s.length == 5 && s !== three && s !== five)
 
   const analysis = {
     zero, one, two, three, four, five, six, seven, eight, nine
   }
 
-  return Object.values(analysis).reduce((dict, v,i) => {dict[sortStr(v)] = i; return dict}, {})
+  return Object.values(analysis).reduce((dict, v,i) => {dict[v.join('')] = i; return dict}, {})
 }
 
 
 function partTwo(lines) {
-let  sum = 0
+  let  sum = 0
   for (const {signalPatterns, outputValues} of lines) {
     const dict = analyze(signalPatterns)
     const num = outputValues.map(s => dict[sortStr(s)])
