1cbdfd9f33417112ecdf1300eab16f05591e3bbb
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-12-08 19:52:01 +0100

    Day 6: Lanternfish in haskell
    linting

--- 06-Lanternfish.hs
+++ 06-Lanternfish.hs
@@ -8,7 +8,7 @@ toi s = read s::Int
 
 -- | Translate a list of fish states into a list of number of fishes per state
 initState :: [Int] -> [Int]
-initState fish = map (\xs -> (length xs - 1)) $ group $ sort (fish ++ [0..8])
+initState fish = map (\xs -> length xs - 1) $ group $ sort (fish ++ [0..8])
 
 -- | Calculate one day iteration
 nextState :: [Int] -> [Int]
@@ -16,21 +16,17 @@ nextState state = shifted ++ [breeders]
   where
     breeders = head state
     rest = tail state
-    shifted = (take 6 rest) ++ [( rest !! 6 ) + breeders] ++ (drop 7 rest)
+    shifted = take 6 rest ++ [( rest !! 6 ) + breeders] ++ drop 7 rest
 
 
 -- | Calculate the next state over a given amount of days
 advanceTime :: Int -> [Int] -> [Int]
-advanceTime days state = foldl (\state _ -> (nextState state)) state [1..days]
-
--- | Sum list
-countFishes :: [Int] -> Int
-countFishes = foldl (+) 0
+advanceTime days state = foldl (\state _ -> nextState state) state [1..days]
 
 main = do
   contents <- getContents
   let input = map toi $ head $ map (splitOn ",") $ lines contents
       initialState = initState input
 
-  print $ countFishes $ advanceTime 80 initialState
-  print $ countFishes $ advanceTime 265 initialState
+  print $ sum $ advanceTime 80 initialState
+  print $ sum $ advanceTime 265 initialState
