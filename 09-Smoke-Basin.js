f32ff03fa0151cf127f147fb951e2bfb3efaf9c9
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-12-09 07:00:21 +0100

    Day 9: Smoke Basin in javascript

---
+++ 09-Smoke-Basin.js
@@ -0,0 +1,61 @@
+
+const readline = require('readline')
+const rl = readline.createInterface({ input: process.stdin })
+
+const lines = []
+let maxRow
+let maxCol
+
+rl.on("line", data => {
+  const line = data.split('').map(i=>Number.parseInt(i))
+  lines.push(line)
+  maxRow = lines.length - 1
+  maxCol = lines[0].length - 1
+})
+
+rl.on('close', () => {
+  console.log({maxRow, maxCol})
+  console.log('partOne', partOne(lines))
+  console.log('partTwo', partTwo(lines))
+})
+
+function cell(row,col) {
+  return lines[row][col]
+}
+
+function isLow(row, col) {
+  const current = cell(row,col)
+  const nums = [current]
+  if (row > 0) {
+    nums.push(cell(row-1, col))
+  }
+  if (row < maxRow) {
+    nums.push(cell(row+1, col))
+  }
+  if (col > 0) {
+    nums.push(cell(row, col-1))
+  }
+  if (col < maxCol) {
+    nums.push(cell(row, col+1))
+  }
+
+  if (nums.filter(n => n === current).length > 1) return false
+  return current === Math.min(...nums)
+}
+
+function partOne(lines) {
+  let sum = 0
+  for (let row = 0; row <= maxRow; row +=1 ) {
+    for (let col = 0; col <= maxCol; col +=1 ) {
+      if (isLow(row, col)) {
+        console.log(row,col)
+        sum+=cell(row, col) + 1
+      }
+    }
+  }
+  return sum
+}
+
+function partTwo(lines) {
+  return 'todo'
+}
