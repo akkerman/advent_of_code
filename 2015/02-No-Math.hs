76d5819928c6330239401b3a9245599b8628e1fd
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-12-26 16:48:36 +0100

    202112261648

--- 2015/02-No-Math.hs
+++ 2015/02-No-Math.hs
@@ -1,16 +1,18 @@
 
 import Data.List.Split (splitOn)
+import Data.List (sort)
 import Utils (atoiList)
 
 squareFt [x,y,z] = minimum sq + sum (map (* 2) sq)
   where sq = [x*y, y*z, z*x]
 
+rLength xs = product xs + 2 * sum (init $ sort xs)
 
 main = do
   contents <- getContents
   let dimensions = map (atoiList "x") (lines contents)
+      wrappingPaper = sum $ map squareFt dimensions
+      ribbonLength = sum $ map rLength dimensions
 
-
-  print $ sum $ map squareFt dimensions
-
-  -- 865832 is too low
+  print wrappingPaper
+  print ribbonLength
