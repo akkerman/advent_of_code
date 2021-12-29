1474daade8aeabe5b33c57f49b4ab79fdf1ef426
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-12-29 08:37:36 +0100

    Day 6: Fire hazard
    Thought it would be fun to explore matrices in haskell
    the solution works on small matrices but does not scale.

--- 2015/06-Fire-Hazard.hs
+++ 2015/06-Fire-Hazard.hs
@@ -1,5 +1,6 @@
-
 import Data.List.Split
+import Data.Matrix
+
 import qualified Data.Set as S
 import Utils
 
@@ -7,30 +8,49 @@ data Operation = ON | OFF | TOGGLE deriving (Eq, Show)
 type Point = (Int, Int)
 type Instruction = (Operation, Point, Point)
 
+parseInput :: [String] -> [Instruction]
 parseInput = map (parseLine.splitOn " ")
  where
-   parseLine ("turn":xs) = parseLine xs
-   parseLine ("on":xs) = parseNums ON xs
-   parseLine ("off":xs) = parseNums OFF xs
-   parseLine ("toggle":xs) = parseNums TOGGLE xs
-   parseNums op [a,_,b] = (op, point a, point b)
-   point = tuple . atoiList'
-   tuple [a,b] = (a, b)
+   parseLine ("turn":"on":xs)   = parseNums ON xs
+   parseLine ("turn":"off":xs)  = parseNums OFF xs
+   parseLine ("toggle":xs)      = parseNums TOGGLE xs
+   parseLine _                  = error "Unknown operation"
 
-points (op, (p1, p2), (q1, q2)) = (op, S.fromList [(p,q) | p <- [p1..q1], q <- [p2..q2]])
+   parseNums op [a,"through",b] = (op, point a, point b)
+   parseNums _ _                = error "unknown interval"
+
+   point = tuple . atoiList'
+
+   tuple [a,b] = (a, b)
+   tuple _     = error "not a tuple"
+
+points :: Instruction -> (Operation, [Point])
+points (op, (p1, p2), (q1, q2)) = (op, [(p,q) | p <- [p1..q1], q <- [p2..q2]])
 
 complement :: Ord a => S.Set a -> S.Set a -> S.Set a
 complement a b = S.difference (S.union a b) (S.intersection a b)
 
 partOne instructions = length $ foldl switch S.empty $ map points instructions
   where
-    switch acc (ON, points) = S.union acc points
-    switch acc (OFF, points) = S.difference acc points
-    switch acc (TOGGLE, points) = complement acc points
+    switch acc (ON, p) = S.union acc (S.fromList p)
+    switch acc (OFF, p) = S.difference acc (S.fromList p)
+    switch acc (TOGGLE, p) = complement acc (S.fromList p)
+
+
+partTwo instructions = foldl switch (zero 1000 1000) $ map points instructions
+  where
+    switch m (ON, p) = foldl (incElem 1) m p
+    switch acc (OFF, p) = foldl decElem acc p
+    switch acc (TOGGLE, p) = foldl (incElem 2) acc p
+    incElem n m coord = setElem ((m ! coord) + n)  coord m
+    decElem m coord = setElem (max ((m ! coord) - 1) 0) coord m
+
 
 main = do
   contents <- getContents
   let input = lines contents
       instructions = parseInput input
 
-  print $ partOne instructions
+  print $ partOne instructions -- 400410
+
+  print $ partTwo instructions
