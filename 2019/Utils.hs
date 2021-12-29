57381f51e8c563de8a1accf4923a0e16d93e466b
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-12-29 11:25:39 +0100

    Day 1: The Tyranny of the Rocket Equation

---
+++ 2019/Utils.hs
@@ -0,0 +1,33 @@
+module Utils where
+
+import Data.List.Split (splitOn)
+
+atoi :: String -> Int
+atoi s = read s::Int
+
+atoiList :: String -> String -> [Int]
+atoiList x = map atoi . splitOn x
+
+atoiList' = atoiList ","
+
+
+-- | split a list in sub lists of given length
+splitlist  :: Int -> [a] -> [[a]]
+splitlist n xs = sp xs
+    where
+        sp [] = []
+        sp xs = take n xs : sp (drop n xs)
+
+-- | flatten a list of list into one list
+flatten :: [[a]] -> [a]
+flatten xss = [x | xs <- xss, x <-xs]
+
+-- | pretty print a list of lists
+pp :: Show a => [[a]] -> IO ()
+pp = putStrLn . unlines . map (unwords . map show)
+
+-- | create one Int from a list
+
+fromDigits :: [Int] -> Int
+fromDigits = foldl addDigit 0
+   where addDigit num d = 10*num + d
