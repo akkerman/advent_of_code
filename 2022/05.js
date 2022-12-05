193521a50cbffb3ecadfc599b9d13e2d4e41850b
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-05 18:01:46 +0100

    202212051801

--- 2022/05.js
+++ 2022/05.js
@@ -1,38 +1,60 @@
 const readline = require('readline')
 const rl = readline.createInterface({ input: process.stdin })
 
+const R = require('ramda')
+
 const instructions = []
-const stack1 = [[]]
-const stack2 = [[]]
+const stack = []
 
 let readStack = true
+
+const instructionRegex = /move (\d+) from (\d+) to (\d+)/
 
 rl.on('line', data => {
   if (data === '') {
     readStack = false
+    stack.pop() // remove the numbers
     return
   }
 
   if (readStack) {
-    const line = data.split('')
-    stack1.push(line)
-    stack2.push([...line])
+    const line = data
+    stack.push(line)
     return
   }
 
-  const line = data
-    .replace('move ', '')
-    .replace('from ', '')
-    .replace('to ', '')
-    .split(/ +/)
-    .map(i => Number.parseInt(i))
-
+  const line = data.match(instructionRegex).slice(1, 4)
   instructions.push(line)
 })
 
+function parseStackRotate (stack) {
+  const newStack = []
+  const size = stack[0].length
+  for (const line of stack) {
+    for (let i = 0; i < size; i += 1) {
+      const elem = line[i]
+      if (' []'.includes(elem)) continue
+      if (!newStack[i]) newStack[i] = []
+      newStack[i].unshift(elem)
+    }
+  }
+
+  return [[], ...newStack.filter(a => a.length)]
+}
+
+const clean = s => s.replace(/[\[ \]]/g, '') // eslint-disable-line 
+
+const parseStackRamda = stack => [[], ...R.pipe(
+  R.map(R.pipe(R.splitEvery(4), R.map(clean))),
+  R.pipe(R.reverse, R.transpose),
+  R.map(R.filter(R.pipe(R.isEmpty, R.not))),
+)(stack)]
+
+const parseStack = parseStackRamda
+
 rl.on('close', () => {
-  console.log('partOne', partOne(stack1, instructions))
-  console.log('partTwo', partTwo(stack2, instructions))
+  console.log('partOne', partOne(parseStack(stack), instructions))
+  console.log('partTwo', partTwo(parseStack(stack), instructions))
 })
 
 function partOne (stack, instructions) {
@@ -47,12 +69,7 @@ function partOne (stack, instructions) {
 
 function partTwo (stack, instructions) {
   for (const [move, from, to] of instructions) {
-    const lifted = []
-    for (let i = 0; i < move; i += 1) {
-      const elem = stack[from].pop()
-      lifted.push(elem)
-    }
-    stack[to] = stack[to].concat(lifted.reverse())
+    stack[to].push(...stack[from].splice(-move))
   }
   return stack.map(s => s.pop()).join('')
 }
