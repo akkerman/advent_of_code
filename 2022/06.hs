e09fe3b30d50707e9527430172c57bb70292207a
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-06 21:07:59 +0100

    202212062107

---
+++ 2022/06.hs
@@ -0,0 +1,23 @@
+module Six () where
+
+import Data.List (nub)
+
+firstSubSet :: Int -> Int -> String -> Int
+firstSubSet start size xs
+  | uniqChars == size = start + size
+  | otherwise = firstSubSet (start + duplicateChars) size (drop duplicateChars xs)
+  where
+    uniqChars = length $ nub (take size xs)
+    duplicateChars = size - uniqChars
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let input = lines contents
+      stream = head input
+
+  print "partOne"
+  print $ firstSubSet 0 4 stream
+
+  print "partTwo"
+  print $ firstSubSet 0 14 stream
