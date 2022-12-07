10419c04d4dd66bfe2175f33fa1ee27e47d74c9f
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-07 09:52:57 +0100

    202212070952

---
+++ 2022/07.js
@@ -0,0 +1,78 @@
+const readline = require('readline')
+const rl = readline.createInterface({ input: process.stdin })
+
+const lines = []
+
+rl.on('line', data => {
+  const line = data
+
+  lines.push(line)
+})
+
+rl.on('close', () => {
+  console.log('partOne', partOne(list(lines)))
+  console.log('partTwo', partTwo(list(lines)))
+})
+
+function list (lines) {
+  let path = []
+  let listing = false
+  const dirs = {}
+
+  for (const line of lines) {
+    if (line.includes('$ cd')) {
+      listing = false
+      const dir = line.split(' ').pop()
+      if (dir === '/') {
+        path = []
+      } else if (dir === '..') {
+        path.pop()
+      } else {
+        path.push(dir)
+      }
+      continue
+    }
+
+    if (line.includes('$ ls')) {
+      listing = true
+      continue
+    }
+    if (listing) {
+      const [size, name] = line.split(' ')
+      if (size === 'dir') { // oeps vergeten
+        const pathstr = [...path, name].join('/')
+        if (!dirs[pathstr]) dirs[pathstr] = 0
+        continue
+      }
+
+      const pathstr = path.join('/')
+
+      if (!dirs[pathstr]) {
+        dirs[pathstr] = 0
+      }
+
+      dirs[pathstr] += parseInt(size)
+    }
+  }
+
+  return dirs
+}
+
+const sort = dirs => Object.keys(dirs).sort((a, b) => b.split('/').length - a.split('/').length)
+
+function partOne (dirs) {
+  for (const path of sort(dirs)) {
+    if (path === '') continue
+    const parent = path.split('/').slice(0, -1).join('/')
+    if (!dirs[parent]) dirs[parent] = 0
+    dirs[parent] += dirs[path]
+  }
+
+  delete dirs['']
+
+  return Object.values(dirs).filter(size => size <= 100000).reduce((a, b) => a + b)
+}
+
+function partTwo (lines) {
+  return '//todo'
+}
