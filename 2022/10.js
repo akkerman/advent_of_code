29c8b7022288fb2fd493e0ca9b9cfbe175f0b5c9
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-10 07:07:01 +0100

    202212100707

--- 2022/10.js
+++ 2022/10.js
@@ -1,6 +1,8 @@
 const readline = require('readline')
 const rl = readline.createInterface({ input: process.stdin })
 
+const R = require('ramda')
+
 const lines = []
 
 rl.on('line', data => {
@@ -40,6 +42,27 @@ function partOne (instructions) {
   return strength
 }
 
-function partTwo (lines) {
-  return 'todo'
+function partTwo (instructions) {
+  const crt = []
+  let registerX = 1
+  let cycles = 0
+  for (const instruction of instructions) {
+    function check () {
+      if ([registerX - 1, registerX, registerX + 1].includes(cycles % 40)) {
+        crt.push('#')
+      } else {
+        crt.push('.')
+      }
+      cycles += 1
+    }
+
+    check()
+
+    if (!instruction.includes('noop')) {
+      check()
+      const add = parseInt(instruction.split(' ')[1])
+      registerX += add
+    }
+  }
+  return R.map(R.join(''), R.splitEvery(40, crt))
 }
