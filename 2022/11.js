1561a9e1898551af46323731d771aab95e4ae0e1
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-11 19:56:55 +0100

    202212111956

--- 2022/11.js
+++ 2022/11.js
@@ -1,6 +1,8 @@
 const readline = require('readline')
 const rl = readline.createInterface({ input: process.stdin })
 
+const R = require('ramda')
+
 let lines = []
 const monkeys = []
 
@@ -10,8 +12,8 @@ function parseMonkey (lines) {
     items: lines[idx++].split(': ')[1].split(', ').map(i => parseInt(i)),
     operation: lines[idx++].split('new = ')[1],
     test: parseInt(lines[idx++].split('by ')[1]),
-    ifTrue: parseInt(lines[idx++].split('monkey ')[1]),
-    ifFalse: parseInt(lines[idx++].split('monkey ')[1]),
+    true: parseInt(lines[idx++].split('monkey ')[1]),
+    false: parseInt(lines[idx++].split('monkey ')[1]),
     inspections: 0,
   }
 }
@@ -28,30 +30,32 @@ rl.on('line', data => {
 })
 
 rl.on('close', () => {
-  console.log('partOne', partOne(monkeys))
-  console.log('partTwo', partTwo(monkeys))
+  console.log('partOne', solve(R.clone(monkeys), 20, 3))
+  console.log('partTwo', solve(R.clone(monkeys), 10000, 1))
 })
 
-function partOne (monkeys) {
-  for (let round = 1; round <= 20; round += 1) {
+// zou kleinste gemene meervoud ook werken? hoe bepaal ik dat?
+const lcm = R.compose(R.product, R.uniq, R.map(R.prop('test')))
+
+function solve (monkeys, rounds, div) {
+  const MOD = lcm(monkeys)
+
+  for (let round = 1; round <= rounds; round += 1) {
     for (const monkey of monkeys) {
       const length = monkey.items.length
       for (let i = 0; i < length; i += 1) {
         const old = monkey.items.shift()
         monkey.inspections += 1
-        const worrylevel = Math.floor(eval(monkey.operation) / 3) // eslint-disable-line 
-        const nextMonkey = (worrylevel % monkey.test === 0) ? monkey.ifTrue : monkey.ifFalse
+        let worrylevel = Math.floor(eval(monkey.operation) / div)
+        worrylevel = worrylevel % MOD
+
+        const nextMonkey = monkey[worrylevel % monkey.test === 0]
         monkeys[nextMonkey].items.push(worrylevel)
       }
     }
-    // console.log(round, monkeys)
   }
 
   // always use own comparator, otherwise it is sorted alphabetically!!
   const [first, second] = monkeys.map(m => m.inspections).sort((a, b) => b - a)
   return first * second
-}
-
-function partTwo (monkeys) {
-  return 'todo'
 }
