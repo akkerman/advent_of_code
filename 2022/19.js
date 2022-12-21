465c8ba0c9355792eae7316d842287ce295a0a98
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-21 21:53:45 +0100

    202212212153

--- 2022/19.js
+++ 2022/19.js
@@ -9,7 +9,7 @@ rl.on('line', data => {
     .trim()
     .replaceAll(/ +/g, ',')
 
-  lines.push(JSON.parse(`[${line}]`))
+  lines.push(bluePrint(JSON.parse(`[${line}]`)))
 })
 
 rl.on('close', () => {
@@ -17,20 +17,80 @@ rl.on('close', () => {
   console.log('partTwo', partTwo(lines))
 })
 
-function bluePrint ([id, ore, clay, obsOre, obsClay, geoOre, geoObs]) {
+const resourceTypes = ['obsidian', 'ore', 'clay']
+
+function bluePrint ([id, oreOre, clayOre, obsOre, obsClay, geoOre, geoObs]) {
   return {
     id,
     robots: [
-      { type: 'ore', ore, clay: 0, obsidian: 0 },
-      { type: 'clay', ore: 0, clay, obsidian: 0 },
+      { type: 'ore', ore: oreOre, clay: 0, obsidian: 0 },
+      { type: 'clay', ore: clayOre, clay: 0, obsidian: 0 },
       { type: 'obsidian', ore: obsOre, clay: obsClay, obsidian: 0 },
       { type: 'geode', ore: geoOre, clay: 0, obsidian: geoObs },
     ],
   }
 }
 
-function partOne (lines) {
-  return 'todo'
+function maxGeodes (blueprint) {
+  const initialRobots = { ore: 1, clay: 0, obsidian: 0, geode: 0 }
+  const initialResources = { ore: 0, clay: 0, obsidian: 0, geode: 0 }
+  const maxSpend = blueprint.robots.reduce((cost, robot) => {
+    for (const type of resourceTypes) {
+      cost[type] = Math.max(cost[type] || 0, robot[type] || 0)
+    }
+    return cost
+  }, {
+  })
+
+  const cache = new Map()
+
+  return dfs(24, initialRobots, initialResources)
+
+  function dfs (time, bots, resources) {
+    const key = JSON.stringify({ time, bots, resources })
+    if (cache.has(key)) {
+      return cache.get(key)
+    }
+    if (time <= 0) {
+      return resources.geode
+    }
+
+    // console.log(time, bots, resources)
+
+    let maxGeodes = bots.geode * time + resources.geode
+
+    for (const currentBot of blueprint.robots) {
+      const newBots = { ...bots }
+      const newResources = { ...resources }
+      for (const type of ['geode', ...resourceTypes]) {
+        newResources[type] += bots[type]
+      }
+
+      // do we have the resources to build this bot
+      const canBuildBot = resourceTypes.reduce((canBuild, type) => canBuild && currentBot[type] <= resources[type], true)
+      // we always want a geode bot, but we do not need to gather more resources in a minute than we can spend in a minute
+      const wantBuildBot = currentBot.type === 'geode' || bots[currentBot.type] < maxSpend[currentBot.type]
+
+      if (wantBuildBot && canBuildBot) {
+        for (const type of resourceTypes) {
+          newResources[type] -= currentBot[type]
+        }
+        newBots[currentBot.type] += 1
+      }
+      const result = dfs(time - 1, newBots, newResources)
+      cache.set(key, result)
+
+      maxGeodes = Math.max(maxGeodes, result)
+    }
+
+    return maxGeodes
+  }
+}
+
+function partOne (blueprints) {
+  const mx = blueprints.map(maxGeodes)
+  console.log(mx)
+  return mx.map((m, i) => (i + 1) * m).reduce((a, b) => a + b)
 }
 
 function partTwo (lines) {
