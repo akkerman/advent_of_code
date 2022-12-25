2c0dedb6f0a5eb60456ceb95dd4dc9a55cb4f801
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-25 20:21:28 +0100

    202212252021

--- 2016/03.js
+++ 2016/03.js
@@ -1,27 +1,37 @@
 const readline = require('readline')
 const rl = readline.createInterface({ input: process.stdin })
 
+const R = require('ramda')
+
 const lines = []
+
+const columns = [[], [], []]
 
 rl.on('line', data => {
   const line = data.trim().replace(/ +/g, ' ').split(' ').map(i => parseInt(i))
   lines.push(line)
+
+  for (let i = 0; i <= 2; i += 1) {
+    columns[i].push(line[i])
+  }
 })
 
 rl.on('close', () => {
   console.log('partOne', partOne(lines))
-  console.log('partTwo', partTwo(lines))
+  console.log('partTwo', partTwo(columns))
 })
 
 function partOne (lines) {
-  let sum=0
-  for (line of lines){
-    const [a,b,c]=   line.sort((a,b)=>a-b)
-    if (a+b > c) sum+=1
+  let sum = 0
+  for (const line of lines) {
+    const [a, b, c] = line.sort((a, b) => a - b)
+    if (a + b > c) sum += 1
   }
   return sum
 }
 
-function partTwo (lines) {
-  return 'todo'
+function partTwo (columns) {
+  const lines = R.splitEvery(3, [].concat(...columns))
+
+  return partOne(lines)
 }
