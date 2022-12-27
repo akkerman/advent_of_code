656640eface7c7084be3dc24b5f268cda333408d
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-27 07:50:56 +0100

    202212270750

--- 2020/04.py
+++ 2020/04.py
@@ -3,18 +3,10 @@
 import sys
 import string
 
+import regex as re
 
-expected_fields = set([
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid',
-    'cid',
-    ])
 
+expected_fields = {'byr', 'iyr', 'eyr', 'hcl', 'ecl', 'pid', 'cid', 'hgt'}
 optional_fields = {'cid'}
 
 
@@ -24,21 +16,79 @@ def passport_fields(line):
         fields.add(field.split(':')[0])
     return fields
 
+def has_valid_fields(fields): 
+    return fields == expected_fields or (expected_fields - fields) == optional_fields
 
-def part_one(lines): 
+
+def as_passport(line):
+    passport = {}
+    for field in line.strip().split(' '):
+        key, value = field.split(':')
+        passport[key] = value
+    return passport
+
+
+def is_valid_passport(passport):
+    byr = int(passport['byr'])
+    if not 1920 <= byr <= 2002:
+        return False
+
+    iyr = int(passport['iyr'])
+    if not 2010 <= iyr <= 2020:
+        return False
+
+    eyr = int(passport['eyr'])
+    if not 2020 <= eyr <= 2030:
+        return False
+
+    hgt = passport['hgt']
+    if re.match(r'^\d+in$', hgt):
+        hgt = int(hgt[0:-2])
+        if not 59 <= hgt <= 76:
+            return False
+    elif re.match(r'^\d+cm$', hgt):
+        hgt = int(hgt[0:-2])
+        if not 150 <= hgt <= 193:
+            return False
+    else:
+        return False
+
+    hcl = passport['hcl']
+    if not re.match(r'^#[0-9a-f]{6}$', hcl):
+        return False
+
+    if passport['ecl'] not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+        return False
+
+    pid = passport['pid']
+    if not re.match(r'^[0-9]{9}$', pid):
+        return False
+
+    return True
+
+
+def part_one(lines):
     valid = 0
     for line in lines:
-        fields = passport_fields(line)
-
-        if (fields == expected_fields or (expected_fields - fields) == optional_fields):
+        if has_valid_fields(passport_fields(line)):
             valid += 1
 
     return valid
 
 
 def part_two(lines):
-    """ part two """
-    return 'todo'
+    valid = 0
+    for line in lines:
+        if not has_valid_fields(passport_fields(line)):
+            continue
+
+        passport = as_passport(line)
+        if not is_valid_passport(passport):
+            continue
+
+        valid += 1
+
+    return valid
 
 
 def main():
