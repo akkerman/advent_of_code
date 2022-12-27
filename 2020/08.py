4d4e502f720a9dffcae1d467ce617f909281ee78
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-27 18:35:13 +0100

    202212271835

--- 2020/08.py
+++ 2020/08.py
@@ -1,7 +1,6 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring
 # pylint: disable=invalid-name
 import sys
-import string
 
 
 def part_one(lines):
@@ -9,7 +8,7 @@ def part_one(lines):
     idx = 0
     acc = 0
     done = set()
-    while True:
+    while idx < len(lines):
         instr, num = lines[idx]
 
         new_acc = acc
@@ -22,27 +21,38 @@ def part_one(lines):
             idx += num
 
         if idx in done:
-            return acc
+            return acc, 'break'
 
         done.add(idx)
         acc = new_acc
 
+    return acc, 'finish'
+
 
 def part_two(lines):
     """ part two """
-    return 'todo'
+    for i, (instr, num) in enumerate(lines):
+        copy = lines[:]
+        if instr == 'nop':
+            copy[i] = ('jmp', num)
+        elif instr == 'jmp':
+            copy[i] = ('nop', num)
+        else:
+            continue
 
+        result, f = part_one(copy)
+        # print(result, f)
+        if f == 'finish':
+            return result, f
+
+    return None
 
 def main():
     """ main """
     lines = []
     for line in sys.stdin:
         instr, num = line.replace('\n', '').split(' ')
-
-        try:
-            lines.append((instr, int(num)))
-        except:
-            lines.append((instr, num))
+        lines.append((instr, int(num)))
 
     print('part_one', part_one(lines))
 
