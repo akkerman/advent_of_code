0ae621a9605c38f58e93896ee81d00e5a6cf46ef
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-27 18:51:34 +0100

    202212271851

---
+++ 2020/01.py
@@ -0,0 +1,30 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring
+# pylint: disable=invalid-name
+import sys
+import string
+
+
+def part_one(lines):
+    """ part one """
+    return [a * b for a in lines for b in lines if (a + b) == 2020][0]
+
+
+def part_two(lines):
+    """ part two """
+    return [a * b * c for a in lines for b in lines for c in lines if (a + b + c) == 2020][0]
+
+
+def main():
+    """ main """
+    lines = []
+    for line in sys.stdin:
+        line = line.replace('\n', '')
+    
+        lines.append(int(line))
+
+    print('part_one', part_one(lines))
+
+    print('part_two', part_two(lines))
+
+
+main()
