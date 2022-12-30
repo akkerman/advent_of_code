23b2b01b8364f09ecea777409661b26f33e03c78
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-12-30 06:50:05 +0100

    202212300650

---
+++ 2020/07.py
@@ -0,0 +1,49 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring
+# pylint: disable=invalid-name
+from collections import defaultdict, deque
+import sys
+
+
+def part_one(lines):
+    """ part one """
+    contained_in = defaultdict(list)
+    for bag, contains in lines:
+        for _, contained in contains:
+            contained_in[contained].append(bag)
+
+    # BFS
+    q = deque(['shiny gold'])
+    visited = set()
+    while q:
+        bag = q.popleft()
+        visited.add(bag)
+        for container in contained_in[bag]:
+            q.append(container)
+
+    return len(visited) - 1  # 'shiny gold' does not contian itself
+
+
+def part_two(lines):
+    """ part two """
+    return 'todo'
+
+
+def main():
+    """ main """
+    lines = []
+    for line in sys.stdin:
+        line = line.replace('\n', '')
+        line = line.replace(' bags.', '').replace(' bags', '')
+        line = line.replace(' bag.', '').replace(' bag', '')
+        line = line.replace('no other', '0 other')
+
+        bag, contains = line.split(' contain ')
+        contains = [(int(s[0]), s[2:]) for s in contains.split(', ')]
+
+        lines.append((bag, contains))
+
+    print('part_one', part_one(lines))
+    print('part_two', part_two(lines))
+
+
+main()
