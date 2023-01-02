35f213ea51a7ee576563cdc6ae8a330b323e3ce1
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2023-01-02 16:08:46 +0100

    202301021608

--- 2020/14.py
+++ 2020/14.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name
 import sys
 
+
 def part_one(lines):
     """ part one """
     mask = ''
@@ -18,7 +19,21 @@ def part_one(lines):
 
 def part_two(lines):
     """ part two """
-    return 'todo'
+
+    mask = ''
+    mem = {}
+    for what, value in lines:
+        if what == 'mask':
+            mask = value
+            continue
+
+        address = int_to_bits(what.replace('mem[', '').replace(']', ''))
+        address = overwrite_x(mask, address)
+
+        for addr in generate_addresses(address):
+            mem[addr] = int(value)
+
+    return sum(mem.values())
 
 
 def int_to_bits(num):
@@ -40,8 +55,33 @@ def overwrite(mask, value):
     return result
 
 
+def overwrite_x(mask, value):
+    result = ""
+    for m, v in zip(mask, value):
+        if m == '0':
+            result += v
+        elif m == '1':
+            result += '1'
+        elif m == 'X':
+            result += 'X'
+        else:
+            assert False
+
+    return result
+
+
 def overwrite_int(mask, value):
     return bits_to_int(overwrite(mask, int_to_bits(value)))
+
+
+def generate_addresses(address):
+    if 'X' not in address:
+        yield address
+    else:
+        for x0 in generate_addresses(address.replace('X', '0', 1)):
+            yield x0
+        for x1 in generate_addresses(address.replace('X', '1', 1)):
+            yield x1
 
 
 def main():
@@ -52,6 +92,7 @@ def main():
         lines.append(line)
 
     print('part_one', part_one(lines))
+
 
     print('part_two', part_two(lines))
 
