2421da8e38ee1880b3747a4724d25cd3594dc9a0
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2023-12-08 18:58:32 +0100

    202312081858

--- 2023/01.lua
+++ 2023/01.lua
@@ -8,8 +8,27 @@ function partOne (lines)
     return sum
 end
 
+translations = {
+  one = "one1one",
+  two = "two2two",
+  three = "three3three",
+  four = "four4four",
+  five = "five5five",
+  six = "six6six",
+  seven = "seven7seven",
+  eight = "eight8eight",
+  nine = "nine9nine",
+}
+
 function partTwo (lines) 
-    return 'todo'
+    translated = {}
+    for _,line in pairs(lines) do
+        for n,r in pairs(translations) do
+            line = string.gsub(line,n,r)
+        end
+        table.insert(translated, line)
+    end
+    return partOne(translated)
 end
 
 function main() 
