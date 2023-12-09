b72ae2adda2b4f47d7aa9de51cea644f22ffd7ac
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2023-12-09 11:36:12 +0100

    202312091136

--- 2023/09.lua
+++ 2023/09.lua
@@ -34,8 +34,31 @@ function partOne (lines)
     return sum
 end
 
+function sequencer2 (history)
+    local fst = {}
+    local h = history
+    while true do
+       table.insert(fst, h[1]) 
+       local nh = {}
+       for i=2, #h do
+           table.insert(nh, h[i] - h[i-1])
+       end
+       h = nh
+       if allZeros (h) then break end
+    end
+    local seqSum = 0
+    for i = #fst, 1, -1 do
+        seqSum = fst[i] - seqSum
+    end
+    return seqSum
+end 
 function partTwo (lines) 
-    return 'todo'
+    local sum = 0
+    for k,history in pairs(lines) do
+        ans = sequencer2(history) 
+        sum = sum + ans
+    end
+    return sum
 end
 
 function main() 
