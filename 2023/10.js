e723936d299f3a36681da9b05c0a1bd46f7f554b
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2023-12-10 19:59:11 +0100

    202312101959

--- 2023/10.js
+++ 2023/10.js
@@ -75,6 +75,7 @@ function getNeighbours (graph, node) {
   return neighbours.filter(isInside(graph)).map(coord => ({ coord, cost: 1, c: graph[coord[0]][coord[1]] }))
 }
 
+let pipes
 function search (graph, start) {
   const queue = [{ coord: start, cost: 0 }]
   const visited = {
@@ -99,18 +100,75 @@ function search (graph, start) {
       }
       queue.push(updated)
       visited.add(updated)
+
       cost = Math.max(cost, updated.cost)
     }
   }
-
+  pipes = visited.nodes
   return cost
 }
 
+function intersectsHorizontal (graph, start) {
+  const [r, c] = start
+
+  let i = 0
+  let pc // previous character
+  for (let dc = c + 1; dc < graph[0].length; dc += 1) {
+    if (!pipes.has([r, dc].toString())) continue
+    const ch = graph[r][dc]
+    if (ch === '-') continue
+    if (pc === 'F' && ch === 'J') continue
+    if (pc === 'L' && ch === '7') continue
+    i += 1
+    pc = ch
+  }
+  return i
+}
+
+function intersectVertical (graph, start) {
+  const [r, c] = start
+
+  let i = 0
+  let pc // previous character
+  for (let dr = r + 1; dr < graph.length; dr += 1) {
+    if (!pipes.has([dr, c].toString())) continue
+    const ch = graph[dr][c]
+    if (ch === '|') continue
+    if (pc === '7' && ch === 'L') continue
+    if (pc === 'F' && ch === 'J') continue
+    i += 1
+    pc = ch
+  }
+  return i
+}
+
+function isInsidePipes (i) {
+  return i % 2 === 1
+}
+
 function partOne (graph, start) {
-  // too low -> 6796
   return search(graph, start)
 }
 
-function partTwo (lines) {
-  return 'todo'
+function partTwo (graph) {
+  // too high -> 501
+  let i = 0
+
+  for (let r = 0; r < graph.length; r += 1) {
+    for (let c = 0; c < graph[0].length; c += 1) {
+      const coord = [r, c].toString()
+      if (!pipes.has(coord)) {
+        const ih = intersectsHorizontal(graph, [r, c])
+        const iv = intersectVertical(graph, [r, c])
+        if (isInsidePipes(ih)) {
+          if (!isInsidePipes(iv)) {
+            log([r, c], ih, iv)
+            throw new Error('different answers from H and V')
+          }
+          i += 1
+        }
+      }
+    }
+  }
+  return i
 }
