77c72c3962cde1e7a6256ac3b5caeba06a96995f
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2023-12-14 18:50:07 +0100

    202312141850

--- 2023/14.js
+++ 2023/14.js
@@ -1,12 +1,7 @@
-const R = require('ramda')
 const readline = require('readline')
 const { newCoordSet, findOccurences } = require('../utils.js')
 const rl = readline.createInterface({ input: process.stdin })
-const tap = R.tap
-const log = console.log // eslint-disable-line
 const sum = (a,b) => a+b // eslint-disable-line
-const mul = (a,b) => a*b // eslint-disable-line
-const int = R.pipe(R.trim, parseInt) // eslint-disable-line
 
 /**
  * @typedef {import('../utils').CoordSet} CoordSet
@@ -44,7 +39,7 @@ function main () {
 
   rl.on('close', () => {
     console.log('partOne', partOne(index(lines)))
-    console.log('partTwo', partTwo(index(lines)))
+    console.log('partTwo', partTwo({ lines, ...index(lines) }))
   })
 }
 
@@ -76,6 +71,121 @@ function partOne ({ roundRocks, cubeRocks }) {
     .reduce(sum)
 }
 
-function partTwo (lines) {
-  return 'todo'
+/** @param {{
+ * lines: string[]
+ * roundRocks: CoordSet
+ * cubeRocks: CoordSet
+ * }} _
+ * @returns {number}
+ */
+function partTwo ({ lines, roundRocks, cubeRocks }) {
+  const maxRow = lines.length
+  const maxCol = lines[0].length - 1
+  const cache = new Map()
+
+  const toString = coords => JSON.stringify(
+    coords.sort(([a1, a2], [b1, b2]) => (100 * a1 + a2) - (100 * b1 + b2)),
+  )
+
+  function cycle (coords) {
+    const coordsStr = toString(coords)
+    if (cache.has(coordsStr)) {
+      return [cache.get(coordsStr), true]
+    }
+    roundRocks = newCoordSet()
+    coords.forEach(roundRocks.add)
+
+    // NORTH ====================
+    let rocks = [...roundRocks.values()].sort(([a], [b]) => b - a)
+
+    for (const rock of rocks) {
+      let [row, col] = rock
+      while (row < maxRow) {
+        if (roundRocks.has([row + 1, col]) || cubeRocks.has([row + 1, col])) {
+          break
+        }
+        row += 1
+      }
+      roundRocks.delete(rock)
+      roundRocks.add([row, col])
+    }
+
+    // WEST ====================
+    rocks = [...roundRocks.values()].sort(([, a], [, b]) => a - b)
+
+    for (const rock of rocks) {
+      let [row, col] = rock
+      while (col > 0) {
+        if (roundRocks.has([row, col - 1]) || cubeRocks.has([row, col - 1])) {
+          break
+        }
+        col -= 1
+      }
+      roundRocks.delete(rock)
+      roundRocks.add([row, col])
+    }
+
+    // SOUTH ====================
+    rocks = [...roundRocks.values()].sort(([a], [b]) => a - b)
+
+    for (const rock of rocks) {
+      let [row, col] = rock
+      while (row > 1) {
+        if (roundRocks.has([row - 1, col]) || cubeRocks.has([row - 1, col])) {
+          break
+        }
+        row -= 1
+      }
+      roundRocks.delete(rock)
+      roundRocks.add([row, col])
+    }
+
+    // EAST ====================
+    rocks = [...roundRocks.values()].sort(([, a], [, b]) => b - a)
+
+    for (const rock of rocks) {
+      let [row, col] = rock
+      while (col < maxCol) {
+        if (roundRocks.has([row, col + 1]) || cubeRocks.has([row, col + 1])) {
+          break
+        }
+        col += 1
+      }
+      roundRocks.delete(rock)
+      roundRocks.add([row, col])
+    }
+
+    coords = [...roundRocks.coords.values()]
+
+    cache.set(coordsStr, coords)
+
+    return [coords, false]
+  }
+
+  let coords = [...roundRocks.coords.values()]
+
+  const cycles = 1000000000
+  const iterations = new Map()
+  for (let i = 1; i <= cycles; i += 1) {
+    const [c, repeat] = cycle(coords)
+    coords = c
+    let firstIteraton = 0
+    if (repeat) {
+      if (firstIteraton === 0) { firstIteraton = i }
+      const s = toString(coords)
+      if (iterations.has(s)) {
+        const idx = (cycles - firstIteraton) % iterations.size
+
+        const coords = [...iterations.keys()][idx]
+        return JSON.parse(coords)
+          .map(([row]) => row)
+          .reduce(sum)
+      }
+      iterations.set(s, i)
+    }
+  }
+
+  return coords
+    .map(([row]) => row)
+    .reduce(sum)
 }
