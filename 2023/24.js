0f1e2cc0fc60c934675f3698a35af48b2ef77729
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2023-12-27 10:14:43 +0100

    202312271014

--- 2023/24.js
+++ 2023/24.js
@@ -7,8 +7,18 @@ const sum = (a,b) => a+b // eslint-disable-line
 const mul = (a,b) => a*b // eslint-disable-line
 const int = R.pipe(R.trim, parseInt) // eslint-disable-line
 
+/**
+ * @typedef {[number,number,number]} Coord
+ * @typedef {{
+ * id:number
+ * pos: Coord
+ * vel: Coord
+ * }} Stone
+ */
+
 function main () {
-  const storms = []
+  /** @type {Stone[]} */
+  const stones = []
 
   let maxX = Number.MIN_SAFE_INTEGER
   let minX = Number.MAX_SAFE_INTEGER
@@ -24,11 +34,10 @@ function main () {
 
     const pos = p.split(', ').map(int)
     const vel = v.split(', ').map(int)
-    storms.push({
+    stones.push({
       id,
       pos,
       vel,
-      crosses: [],
     })
 
     const [x, y, z] = pos
@@ -42,63 +51,64 @@ function main () {
   })
 
   rl.on('close', () => {
-    const area = storms.length === 5
+    const area = stones.length === 5
       ? [7, 27]
       : [200000000000000, 400000000000000]
 
-    const minmax = { maxX, minX, maxY, minY, maxZ, minZ }
-    const length = [maxX + minX, maxY + minY, maxZ + minZ]
-    console.log('partOne', partOne(storms, area, length))
-    console.log('partTwo', partTwo(storms, area, length))
+    console.log('partOne', partOne(stones, area))
+    console.log('partTwo', partTwo(stones, area))
   })
 }
 
 main()
 
+/** @type {number[][] => bigint[][]} */
+const toBigInt = line => line.map(coord => coord.map(BigInt))
+
 /**
- * @type {[Coord,Coord]} line1
- * @type {[Coord,Coord]} line2
+ * @param {[Coord,Coord]} line1
+ * @param {[Coord,Coord]} line2
+ * @returns {[number,number]|undefined}
  */
 function intersect (line1, line2) {
+  line1 = toBigInt(line1)
+  line2 = toBigInt(line2)
   // https://stackoverflow.com/a/20677983/3889449
   const xdiff = [line1[0][0] - line1[1][0], line2[0][0] - line2[1][0]]
   const ydiff = [line1[0][1] - line1[1][1], line2[0][1] - line2[1][1]]
 
-  // GVD!!
-  const det = (a, b) => parseInt(BigInt(a[0]) * BigInt(b[1]) - BigInt(a[1]) * BigInt(b[0]))
+  const det = (a, b) => a[0] * b[1] - a[1] * b[0]
 
   const div = det(xdiff, ydiff)
-  if (div === 0) { return null }
+  if (div === BigInt(0)) {
+    return null // doesn't intersect
+  }
 
   const d = [det(line1[0], line1[1]), det(line2[0], line2[1])]
   const x = det(d, xdiff) / div
   const y = det(d, ydiff) / div
-  return [x, y]
+  return [parseInt(x), parseInt(y)]
 }
 
 /** @type {(coord:Coord, dir:Coord) => Coord} */
 const add = (coord, dir) => R.zipWith(sum, coord, dir)
 
-const sub = (coord, dir) => R.zipWith((a, b) => a - b, coord, dir)
+/** @type {(from:Coord, to:Coord) => Coord} */
+const direction = (from, to) => R.zipWith((a, b) => b - a, from, to)
 
-const direction = (from, to) => {
-  const retval = R.zipWith((a, b) => b - a, from, to)
-  return retval
-}
-
-const sameDirection = ([x1, y1], [x2, y2]) => {
-  const retval = Math.sign(x1) === Math.sign(x2) &&
+/** @type {(from:Coord, to:Coord) => boolean} */
+const sameDirection = ([x1, y1], [x2, y2]) =>
+  Math.sign(x1) === Math.sign(x2) &&
   Math.sign(y1) === Math.sign(y2)
-  return retval
-}
 
-function partOne (storms, [min, max], lengths) {
+/** @type {(stones:Stone[], [min,max]:[number,number]) => number} */
+function partOne (stones, [min, max]) {
   let sum = 0
-  for (let i = 0; i < storms.length; i += 1) {
-    for (let j = i + 1; j < storms.length; j += 1) {
-      if (i === j) continue // same storm
-      const { id: id1, pos: p1, vel: v1 } = storms[i]
-      const { id: id2, pos: p2, vel: v2 } = storms[j]
+  for (let i = 0; i < stones.length; i += 1) {
+    for (let j = i + 1; j < stones.length; j += 1) {
+      if (i === j) continue // same stone
+      const { pos: p1, vel: v1 } = stones[i]
+      const { pos: p2, vel: v2 } = stones[j]
       const cross = intersect([p1, add(p1, v1)], [p2, add(p2, v2)])
 
       if (cross) {
@@ -106,28 +116,19 @@ function partOne (storms, [min, max], lengths) {
         if (min <= x && x <= max &&
            min <= y && y <= max) {
           // log('in test area')
-          const crossI = { id2, cross, future: false }
-          const crossJ = { id1, cross, future: false }
           if (sameDirection(direction(p1, cross), v1) &&
               sameDirection(direction(p2, cross), v2)) {
             // log('in future')
-            crossI.future = true
-            crossJ.future = true
-
             sum += 1
           }
-          storms[i].crosses.push(crossI)
         }
       }
     }
   }
 
-  // storms.slice(0, 1).forEach(({ crosses, ...rest }) => log('storm', rest, 'crosses', crosses))
-
-  // too low 21662
   return sum
 }
 
-function partTwo (storms, { min, max }) {
+function partTwo (storms, [min, max]) {
   return 'todo'
 }
