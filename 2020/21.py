f0e594079f9ed3f64a2c9c802ff3a383d9bef1cd
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-02 14:58:07 +0100

    202401021458

--- 2020/21.py
+++ 2020/21.py
@@ -3,7 +3,7 @@
 import sys
 import string
 import re
-from collections import Counter
+from collections import Counter, deque
 
 
 def part_one(foods):
@@ -24,22 +24,51 @@ def part_one(foods):
         over = set.intersection(*ingredient_sets)
         no_allergens -= over
 
-    print(no_allergens)
-
     count = 0
     for ingredient in no_allergens:
-        print(ingredient, ingredient_count[ingredient])
         count += ingredient_count[ingredient]
 
     return count
 
 
-
-
 def part_two(foods):
     """ part two """
+    allergen_count = Counter()
+    ingredient_count = Counter()
 
-    return "todo"
+    for ingredients,_ in foods:
+        ingredient_count.update(ingredients)
+
+    for _, allergens in foods:
+        allergen_count.update(allergens)
+
+    no_allergens = set(ingredient_count.keys())
+
+    for allergen, _ in allergen_count.most_common():
+        ingredient_sets = [set(ing) for (ing, allergens) in foods if allergen in allergens]
+        over = set.intersection(*ingredient_sets)
+        no_allergens -= over
+
+    allergen2ingredient = dict()
+    candidates = ingredient_count.keys() - no_allergens
+    queue = deque(allergen for (allergen, _) in allergen_count.most_common())
+
+    while len(queue):
+        allergen = queue.popleft()
+        ingredient_sets = [set(ing) for (ing, allergens) in foods if allergen in allergens]
+        over = set.intersection(*ingredient_sets)
+        over = over - (over - candidates)
+        if len(over) == 1:
+            i = over.pop()
+            allergen2ingredient[allergen] = i
+            candidates.remove(i)
+        else:
+            queue.append(allergen)
+
+
+    
+
+    return ",".join([allergen2ingredient[key] for key in sorted(allergen2ingredient)])
 
 
 def main():
