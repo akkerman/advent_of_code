17fe4694c6d976ec452e431cb0cb4a16d38ab4af
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-04 20:54:38 +0100

    202401042054

--- 2020/23.py
+++ 2020/23.py
@@ -54,9 +54,129 @@ def part_one(cups):
     return "".join(cs[pos+1:] + cs[:pos])
 
 
-def part_two(cups):
+class Cup:
+    """ representation of a Cup """
+    def __init__(self, value:str):
+        self.value=value
+        self.next=self
+        self.prev=self
+
+    def __repr__(self):
+        return f"{self.prev.value}-({self.value})-{self.next.value}"
+
+def part_two(cupValues):
     """ part two """
-    return 'todo'
+
+    maxValue = 1000000
+
+    lookup = {}
+
+    def init():
+        cups = [Cup(v) for v in cupValues]
+
+        first = cups[0]
+        last = cups[-1]
+        for i,c in enumerate(cups):
+            lookup[c.value] = c
+            if i > 0:
+                c.prev = cups[i-1]
+            if i < len(cups)-1:
+                c.next = cups[i+1]
+
+        if maxValue > 10:
+            for i in range(10, maxValue+1):
+                s = str(i)
+                c = Cup(s)
+                lookup[s] = c
+                if i == 10:
+                    continue
+                prev = lookup[str(i-1)]
+                c.prev = prev
+                prev.next = c
+
+            last.next = lookup['10']
+            lookup['10'] = last
+            last = lookup['1000000']
+
+        first.prev = last
+        last.next = first
+
+        return first
+
+    def pick_three(current:Cup)->Cup:
+        t1 = current.next
+        t2 = t1.next
+        t3 = t2.next
+
+        after = t3.next
+
+        current.next = after
+        after.prev = current
+
+        t1.prev = t1
+        t3.next = t3
+
+        return t1
+
+    def select_destination(current, three):
+        vals = [three.value, three.next.value, three.next.next.value]
+        i = int(current.value) - 1
+
+        while True:
+            if i <= 0:
+                i = maxValue
+            if str(i) in vals:
+                i-=1
+                continue
+
+            return lookup[str(i)]
+
+    def place(dest, three):
+        t1 = three
+        t2 = t1.next
+        t3 = t2.next
+
+        t3.next = dest.next
+        t1.prev = dest
+        dest.next = t1
+
+
+
+    def move(current:Cup)->Cup:
+        print("current:", current.value)
+        print("cups    ", values_from(current))
+        three = pick_three(current)
+
+        print("pick up:", three.value, three.next.value, three.next.next.value)
+        dest = select_destination(current, three)
+        print('destination: ', dest.value)
+        print("place before", values_from(current))
+        place(dest, three)
+        print("place after ", values_from(current))
+        print("place after ", values_from(dest))
+        print("")
+
+        return current.next
+
+
+    def values_from(current):
+        c = current
+        nums = []
+        for _ in range(0,20):
+            nums.append(c.value)
+            c = c.next
+        return " ".join(nums)
+
+
+
+    current = init()
+
+
+    # print(values_from(current))
+    for _ in range(0, 10): # 10000000):
+        current = move(current)
+
+    return values_from(lookup['1'])
 
 
 def main():
