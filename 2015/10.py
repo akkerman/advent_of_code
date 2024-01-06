580e7ed3afcf94b8e59e5c86acfc3122034fa778
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-06 21:44:00 +0100

    202401062144

---
+++ 2015/10.py
@@ -0,0 +1,50 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring
+# pylint: disable=invalid-name
+import sys
+import string
+
+
+def play(sequence):
+    cur = sequence[0]
+    c = 1
+    new=[]
+    for n in sequence[1:]:
+        if n == cur:
+            c+=1
+        else:
+            new.append(str(c))
+            new.append(cur)
+            cur=n
+            c=1
+
+    new.append(str(c))
+    new.append(cur)
+    return new
+            
+
+def part_one(sequence):
+    """ part one """
+    for _ in range(0,40):
+        sequence = play(sequence)
+    return len(sequence)
+
+
+def part_two(lines):
+    """ part two """
+    return 'todo'
+
+
+def main():
+    """ main """
+    lines = []
+    for line in sys.stdin:
+        line = line.replace('\n', '')
+    
+        lines.append(line)
+
+    print('part_one', part_one(list(lines[0])))
+
+    print('part_two', part_two(lines))
+
+
+main()
