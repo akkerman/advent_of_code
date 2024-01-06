9e3b69ef427dbb380bace94ac35a01bbd30fb2fa
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-06 21:31:59 +0100

    202401062131

---
+++ 2015/09.py
@@ -0,0 +1,32 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring
+# pylint: disable=invalid-name
+import sys
+import string
+import re
+from itertools import permutations
+
+def lengths(vertices, edges):
+    l = []
+
+reg = re.compile('(.+) to (.+) = (\d+)')
+def main():
+    """ main """
+    vertices=set()
+    edges={}
+    for line in sys.stdin:
+        line = line.replace('\n', '')
+        f, t, l = reg.match(line).groups()
+
+        vertices.add(f)
+        vertices.add(t)
+        edges[(f,t)] = int(l)
+        edges[(t,f)] = int(l)
+
+    lengths = []
+    for perm in permutations(vertices):
+        lengths.append(sum([edges[ft] for ft in zip(perm, perm[1:])]))
+
+    print('part_one', min(lengths))
+    print('part_two', max(lengths))
+
+main()
