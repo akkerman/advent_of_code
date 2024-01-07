4dace0355a7c7122ca9cd8d254884b0bc4cb8fd9
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-07 11:29:26 +0100

    202401071129

--- 2015/12.py
+++ 2015/12.py
@@ -2,6 +2,19 @@
 # pylint: disable=invalid-name
 import sys
 import re
+from json import loads
+
+def count(struct):
+    if isinstance(struct,int):
+        return struct
+    if isinstance(struct,list):
+        return sum((count(struct) for struct in struct))
+    if not isinstance(struct,dict):
+        return 0
+    if 'red' in struct.values():
+        return 0
+    return count(list(struct.values()))
+
 
 def part_one(lines):
     """ part one """
@@ -11,13 +24,11 @@ def part_one(lines):
         return ' '
     nums = "".join([num_space(n) for n in lines[0]])
     nums = re.split(r' +', nums)
-
     return sum(int(n) for n in nums if n != '')
 
 def part_two(lines):
     """ part two """
-    return 'todo'
-
+    return count(loads(lines[0]))
 
 def main():
     """ main """
