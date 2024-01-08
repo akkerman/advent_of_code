f2e0a8b388d49de933abf0152a07dc7e95f559a8
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-08 22:13:44 +0100

    202401082213

--- 2016/18.py
+++ 2016/18.py
@@ -19,12 +19,15 @@ def neighbours(light):
     ])
 
 
-def part_one(lights_on, lights_off):
-    """ part one """
+def solve(lights_on, lights_off, stuck=set()):
     def step(on, off):
         new_on = set()
         new_off = set()
         for light in on:
+            if light in stuck:
+                new_on.add(light)
+                continue
+
             nbs = neighbours(light)
             intersection = on & nbs
             if len(intersection) in [2,3]:
@@ -52,18 +55,15 @@ def part_one(lights_on, lights_off):
     return len(on)
 
 
-def part_two(on, off):
-    """ part two """
-    return 'todo'
-
-
 def main():
     """ main """
     lights_on = set()
     lights_off = set()
     row = 0
+    max_col = 0
     for line in sys.stdin:
         line = line.replace('\n', '')
+        max_col = len(line) - 1
         for col in find_occurences('#', line):
             lights_on.add((row, col))
         for col in find_occurences(r'\.', line):
@@ -71,9 +71,19 @@ def main():
 
         row += 1
 
-    print('part_one', part_one(lights_on, lights_off))
+    max_row = row - 1
 
-    print('part_two', part_two(lights_on, lights_off))
+    stuck = set([
+        (0,0), (0, max_col),
+        (max_row, 0), (max_row, max_col)
+        ])
+
+
+    print('part_one', solve(lights_on, lights_off))
+
+    lights_on = lights_on | stuck
+    lights_off = lights_off - stuck
+    print('part_two', solve(lights_on, lights_off, stuck))
 
 
 main()
