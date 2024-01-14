569485ab85ccf3a288ba4d27c117089385fff501
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-14 10:35:57 +0100

    202401141035

--- 2015/24.py
+++ 2015/24.py
@@ -34,10 +34,33 @@ def part_one(package_weights):
     """ part one """
     return qa(groups(package_weights)[0])
 
+def groups4(package_weights):
+    weight = sum(package_weights) // 4
+    for len1 in range(2, len(package_weights)):
+        for g1 in combinations(package_weights, len1):
+            if sum(g1) != weight:
+                continue
+            after = [p for p in package_weights if p not in g1]
+            for len2 in range(2, len(after)):
+                for g2 in combinations(after, len2):
+                    if sum(g2) != weight or len(g2) < len(g1):
+                        continue
+                    for len3 in range(3, len(after)):
+                        for g3 in combinations(after, len3):
+                            if sum(g3) != weight or len(g3) < len(g1):
+                                continue
+                            g4 = tuple(p for p in after if p not in g2 and p not in g3)
+                            if sum(g4) != weight or len(g4) < len(g1):
+                                continue
+
+                            return [g1, g2, g3, g4]
+
+    return []
+
 
 def part_two(package_weights):
     """ part two """
-    return 'todo'
+    return qa(groups4(package_weights)[0])
 
 
 def main():
