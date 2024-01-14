285f26f58e880a8ab0acf5663792e308633d6226
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-14 14:37:54 +0100

    202401141437

--- 2016/05.py
+++ 2016/05.py
@@ -14,12 +14,15 @@ def part_one(door_id):
     """ part one """
     password = []
 
+    print('generatating password for door 1...')
+
     i = 0
     while len(password) < 8:
         stuff = door_id + str(i)
         h = md5(stuff)
         if h[:5] == '00000':
             password.append(h[5])
+            print("".join(password))
 
         i+=1
 
@@ -29,7 +32,25 @@ def part_one(door_id):
 
 def part_two(door_id):
     """ part two """
-    return 'todo'
+    password = ["_"] * 8
+    print('generatating password for door 2...')
+
+    i = 0
+    while "_" in password:
+        i+=1
+        stuff = door_id + str(i)
+        h = md5(stuff)
+        if h[:5] == '00000':
+            if h[5] not in '01234567':
+                continue
+            idx = int(h[5])
+            if password[idx] != '_':
+                continue
+            password[idx] = h[6]
+            print("".join(password))
+
+
+    return "".join(password)
 
 
 def main():
@@ -41,7 +62,7 @@ def main():
         break
 
     print('part_one', part_one(door_id))
-
+    print('')
     print('part_two', part_two(door_id))
 
 
