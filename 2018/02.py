481f77ee95f8d21feacfe9ac4068d999a471233a
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-01-16 22:48:44 +0100

    202401162248

---
+++ 2018/02.py
@@ -0,0 +1,36 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring
+# pylint: disable=invalid-name
+import sys
+from collections import Counter
+
+def part_one(box_ids):
+    """ part one """
+    x2 = 0
+    x3 = 0
+    for bid in box_ids:
+        c = Counter(list(bid))
+        if 2 in c.values():
+            x2+=1
+        if 3 in c.values():
+            x3+=1
+    return x2*x3
+
+
+def part_two(box_ids):
+    """ part two """
+    return 'todo'
+
+
+def main():
+    """ main """
+    box_ids = []
+    for line in sys.stdin:
+        line = line.replace('\n', '')
+        box_ids.append(line)
+
+    print('part_one', part_one(box_ids))
+
+    print('part_two', part_two(box_ids))
+
+
+main()
