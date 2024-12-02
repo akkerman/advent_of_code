c534d2bb0ebacdcfb397e46f6f744e0b8c059129
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-02 07:25:21 +0100

    202412020725

--- 2024/02.py
+++ 2024/02.py
@@ -4,19 +4,40 @@ import sys
 from typing import List
 
 def is_save(report: List[int]) -> bool:
-    """ check if the report is save """
+    if not sorted_or_reversed(report):
+        return False
+
+    for l1, l2 in zip(report, report[1:]):
+        if not is_save_level(l1, l2):
+            return False
+
+    return True
+
+def sorted_or_reversed(report: List[int]) -> bool:
     sorted_report = sorted(report)
     reversed_report = sorted(report, reverse=True)
     
     if report != sorted_report and report != list(reversed_report):
         return False 
-
-    for l1, l2 in zip(report, report[1:]):
-        diff = abs(l1 - l2)
-        if diff == 0 or diff > 3:
-            return False
-
     return True
+
+def is_save_level(l1: int, l2: int) -> bool:
+    diff = abs(l1 - l2)
+    if diff == 0 or diff > 3:
+        return False
+    return True
+
+def is_save_with_dampeners(report: List[int]) -> bool:
+    if is_save(report):
+        return True
+
+    for i in range(0, len(report)):
+        dampened = report[:i] + report[i+1:]
+        if is_save(dampened):
+            return True
+
+    return False
+
 
 def part_one(reports: List[List[int]]):
     """ part one """
@@ -24,13 +45,16 @@ def part_one(reports: List[List[int]]):
     for report in reports:
         if is_save(report):
             sum += 1
-
     return sum
 
 
 def part_two(reports: List[List[int]]):
     """ part two """
-    return 'todo'
+    sum = 0
+    for report in reports:
+        if is_save_with_dampeners(report):
+            sum += 1
+    return sum
 
 
 def main():
