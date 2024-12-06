8e5bcd945ea1b9bdabc6d51c42d635bd9799fe99
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-06 08:25:00 +0100

    202412060825

--- 2024/06.py
+++ 2024/06.py
@@ -13,7 +13,7 @@ next_direction = {
 }
 
 
-def part_one(map:List[Coord], guard_pos:Coord, max:Coord) -> int:
+def part_one(map:List[Coord], guard_pos:Coord, max:Coord) -> Set[Coord]:
     """ part one """
     positions: Set[Coord] = set()
     def is_obstacle(pos:Coord) -> bool:
@@ -30,12 +30,35 @@ def part_one(map:List[Coord], guard_pos:Coord, max:Coord) -> int:
             dir = next_direction[dir]
             continue
         if not is_valid(next_pos):
-            return len(positions)
+            return positions
         guard_pos = next_pos
 
-def part_two(list) -> int:
+def part_two(map:List[Coord], guard_pos:Coord, max:Coord, path: Set[Coord]) -> int:
     """ part two """
-    return -1
+
+    possibilities = 0
+    for (r1, c1) in path:
+        for (r2, c2) in path:
+            if r1 >= r2 or c1 >= c2:
+               # skip if not diagonal
+               continue
+
+            if (r1, c2) in path and (r2, c1) in path:
+                num_obstacles = 0
+                # a rectangle was found
+                # check bounding obstacles
+                if (r1-1, c1) in map:
+                    num_obstacles += 1
+                if (r1, c2+1) in map:
+                    num_obstacles += 1
+                if (r2+1, c2) in map:
+                    num_obstacles += 1
+                if (r2, c1-1) in map:
+                    num_obstacles += 1
+                if num_obstacles == 3:
+                    possibilities += 1
+
+    return possibilities
 
 
 
@@ -54,10 +77,11 @@ def main():
         map += [(r, c) for c in range(len(line)) if line[c] == '#']
         r+=1
 
-    # off by one!!
-    print('part_one', part_one(map, guard_pos, (r, max_c)))
+    walking_path = part_one(map, guard_pos, (r, max_c))
+    print('part_one', len(walking_path))
 
-    print('part_two', part_two([]))
+    # too low: 539
+    print('part_two', part_two(map, guard_pos, (r, max_c), walking_path))
 
 
 main()
