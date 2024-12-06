18d9028199b9113f811fb49f4b345fdf687e6bc2
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-06 12:55:36 +0100

    202412061255

--- 2024/06.py
+++ 2024/06.py
@@ -1,15 +1,21 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring
 # pylint: disable=invalid-name
 import sys
-from typing import List, Set, Tuple
+from typing import List, Set, Tuple, Dict
 
 Coord = Tuple[int,int]
+Direction = Tuple[int,int]
 
-next_direction = {
-    (-1,0): (0,1),
-    (0,1): (1,0),
-    (1,0): (0,-1),
-    (0,-1): (-1,0),
+UP: Direction = (-1,0)
+DOWN: Direction = (1,0)
+LEFT: Direction = (0,-1)
+RIGHT: Direction = (0,1)
+
+next_direction: Dict[Direction, Direction] = {
+    UP: RIGHT,
+    RIGHT: DOWN,
+    DOWN: LEFT,
+    LEFT: UP,
 }
 
 def print_map(map:List[Coord], guard_pos:Coord, max:Coord, extra: Set[Coord]):
@@ -25,9 +31,10 @@ def print_map(map:List[Coord], guard_pos:Coord, max:Coord, extra: Set[Coord]):
                 print('.', end='')
         print()
 
-def part_one(map:List[Coord], guard_pos:Coord, max:Coord) -> Set[Coord]:
+def part_one(map:List[Coord], guard_pos:Coord, max:Coord):
     """ part one """
-    positions: Set[Coord] = set()
+    path: Set[Coord] = set()
+    directional_path: Set[Tuple[Coord,Direction]] = set()
     def is_obstacle(pos:Coord) -> bool:
         return pos in map
     def is_valid(pos:Coord) -> bool:
@@ -36,41 +43,55 @@ def part_one(map:List[Coord], guard_pos:Coord, max:Coord) -> Set[Coord]:
     dir = (-1,0)
 
     while True:
-        positions.add(guard_pos)
+        path.add(guard_pos)
+        directional_path.add((guard_pos, dir))
         next_pos = (guard_pos[0] + dir[0], guard_pos[1] + dir[1])
         if is_obstacle(next_pos):
             dir = next_direction[dir]
             continue
         if not is_valid(next_pos):
-            return positions
+            return path, directional_path
         guard_pos = next_pos
 
-def part_two(map:List[Coord], guard_pos:Coord, max:Coord, path: Set[Coord]) -> Set[Coord]:
+def neighbors(path:Set[Tuple[Coord,Direction]],pos:Coord) -> Set[Tuple[Coord,Direction]]:
+    r, c = pos
+    candidates: Set[Tuple[Coord,Direction]] = set([ ((r-1, c), DOWN), ((r+1, c), UP), ((r, c-1), RIGHT), ((r, c+1), LEFT) ])
+    return candidates & path
+
+def stuck_in_loop(map:List[Coord], guard_initial_pos: Tuple[Coord, Direction], max: Coord) -> bool:
+    directional_path: Set[Tuple[Coord,Direction]] = set()
+    def is_obstacle(pos:Coord) -> bool:
+        return pos in map
+    def is_valid(pos:Coord) -> bool:
+        return pos[0] >= 0 and pos[0] < max[0] and pos[1] >= 0 and pos[1] < max[1]
+
+    guard_pos:Coord = guard_initial_pos[0]
+    dir:Direction = guard_initial_pos[1]
+
+    while True:
+        if (guard_pos, dir) in directional_path:
+            return True
+
+        directional_path.add((guard_pos, dir))
+        next_pos: Coord = (guard_pos[0] + dir[0], guard_pos[1] + dir[1])
+        if is_obstacle(next_pos):
+            dir = next_direction[dir]
+            continue
+        if not is_valid(next_pos):
+            return False
+        guard_pos = next_pos
+
+
+def part_two(map:List[Coord], guard_pos: Coord, max:Coord, path: Set[Coord], directional_path: Set[Tuple[Coord,Direction]]):
     """ part two """
 
-    possibilities: Set[Coord] = set()
-    for (r1, c1) in map:
-        for (r2, c2) in map:
-            if r1 < r2 and c1 < c2:
-                # main diagonal
-                p1 = (r1+1, c2+1)
-                p2 = (r2-1, c1-1)
-                if p1 in map and p2 not in map:
-                    possibilities.add(p2)
-                if p1 not in map and p2 in map:
-                    possibilities.add(p1)
-
-            if r1 > r2 and c1 < c2:
-                # anti diagonal
-                p1 = (r2-1, c1+1)
-                p2 = (r1+1, c2-1)
-                if p1 in map and p2 not in map:
-                    possibilities.add(p2)
-                if p1 not in map and p2 in map:
-                    possibilities.add(p1)
-            
-
-    return possibilities
+    posibilities: Set[Coord] = set()
+    for pos in path - {guard_pos}:
+        for neighbor in neighbors(directional_path, pos):
+            updated_map = map + [pos]
+            if stuck_in_loop(updated_map, neighbor, max):
+                posibilities.add(pos)
+    return posibilities
 
 
 
@@ -89,15 +110,14 @@ def main():
         map += [(r, c) for c in range(len(line)) if line[c] == '#']
         r+=1
 
-    walking_path = part_one(map, guard_pos, (r, max_c))
-    print('part_one', len(walking_path))
+    path, directional_path = part_one(map, guard_pos, (r, max_c))
+    print('part_one', len(path))
 
     # too low: 539
     # too high: 13823
     # nope: 7644
-    possibilities = part_two(map, guard_pos, (r, max_c), walking_path)
-    print_map(map, guard_pos, (r, max_c), possibilities)
-    print(possibilities)
+    # nope: 1820
+    possibilities = part_two(map, guard_pos, (r, max_c), path, directional_path)
     print('part_two', len(possibilities))
 
 
