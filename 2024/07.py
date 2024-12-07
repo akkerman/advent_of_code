0094444637c73f07aa12f37afc10e15ef48cbb18
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-07 06:16:09 +0100

    202412070616

--- 2024/07.py
+++ 2024/07.py
@@ -4,10 +4,34 @@ import sys
 import string
 from typing import List, Tuple, Dict 
 
+Equation = Tuple[int, List[int]]
 
-def part_one(lines):
+def can_be_true(value:int, numbers:List[int]):
+    def fn(value:int, current: int, numbers:List[int]):
+        if current == value:
+            return True
+        if current > value:
+            return False
+        if len(numbers) == 0:
+            return False
+        if fn(value, current + numbers[0], numbers[1:]):
+            return True
+        if fn(value, current * numbers[0], numbers[1:]):
+            return True
+        return False
+    return fn(value, numbers[0], numbers[1:])
+
+def part_one(equations:List[Equation]):
     """ part one """
-    return 'todo'
+    sum = 0
+    for value, numbers in equations:
+        print(value, end=" ")
+        if can_be_true(value, numbers):
+            sum += value
+            print('true')
+        else:
+            print('false')
+    return sum
 
 
 def part_two(lines):
@@ -17,15 +41,19 @@ def part_two(lines):
 
 def main():
     """ main """
-    lines:List = []
+    equations:List[Equation] = []
     for line in sys.stdin:
         line = line.replace('\n', '')
 
-        lines.append(line)
+        testvalue, nmbrs = line.split(': ')
 
-    print('part_one', part_one(lines))
+        testvalue = int(testvalue)
+        nmbrs = list(map(int, nmbrs.split(' ')))
+        equations.append((testvalue, nmbrs))
 
-    print('part_two', part_two(lines))
+    print('part_one', part_one(equations))
+
+    print('part_two', part_two(equations))
 
 
 main()
