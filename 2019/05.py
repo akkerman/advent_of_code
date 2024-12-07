346c7550443c3915eacf89d805471bc1cba0ccbe
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-07 21:22:40 +0100

    202412072122

---
+++ 2019/05.py
@@ -0,0 +1,73 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring
+# pylint: disable=invalid-name
+import sys
+from typing import List
+
+def input(): 
+    return 1
+
+def output(value:int):
+    print('diagnostic:', value)
+
+def computer(program: List[int]):
+    """ part one """
+    modes = [0,0,0,0]
+
+    def set_value(offset:int, index:int, value:int):
+        # parameters that an instruction writes to wil never be in immediate mode
+        program[program[offset+index]] = value
+
+    def get_value(offset:int, index:int):
+        if modes[index]:
+            # immediate mode
+            return program[offset+index]
+        else:
+            # position mode
+            return program[program[offset+index]]
+
+    i = 0
+    while i < len(program):
+        p3, p2, p1, o1, o2 = list(str(program[i]).zfill(5))
+        modes = [0, int(p1), int(p2), int(p3)]
+        op = int(o1 + o2)
+        if op == 99:
+            break
+        elif op == 1:
+            set_value(i, 3, get_value(i, 1) + get_value(i, 2))
+            i+=4
+        elif op == 2:
+            set_value(i, 3, get_value(i, 1) * get_value(i, 2))
+            i+=4
+        elif op == 3:
+            set_value(i, 1, int(input()))
+            i+=2
+        elif op == 4:
+            output(get_value(i, 1))
+            i+=2
+        else:
+            ValueError('Unknown opcode {}'.format(op))
+
+
+def part_one(program: List[int]):
+    """ part one """
+    return computer(program)
+
+def part_two(program: List[int]):
+    """ part two """
+    return 'todo'
+
+def main():
+    """ main """
+
+    program: List[int] = []
+    for line in sys.stdin:
+        line = line.strip()
+        program = list(map(int, line.split(',')))
+        
+
+    print('part_one', part_one(program.copy()))
+
+    print('part_two', part_two(program.copy()))
+
+
+main()
