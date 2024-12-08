ea8a504ddedc5053e72346ee1f79725f94780eeb
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-08 17:47:03 +0100

    202412081747

--- 2019/05.py
+++ 2019/05.py
@@ -3,13 +3,11 @@
 import sys
 from typing import List
 
-def input(): 
-    return 1
 
 def output(value:int):
     print('diagnostic:', value)
 
-def computer(program: List[int]):
+def computer(program: List[int], input:int=1):
     """ part one """
     modes = [0,0,0,0]
 
@@ -39,22 +37,38 @@ def computer(program: List[int]):
             set_value(i, 3, get_value(i, 1) * get_value(i, 2))
             i+=4
         elif op == 3:
-            set_value(i, 1, int(input()))
+            set_value(i, 1, input)
             i+=2
         elif op == 4:
             output(get_value(i, 1))
             i+=2
+        elif op == 5:
+            if get_value(i, 1) != 0:
+                i = get_value(i, 2)
+            else:
+                i+=3
+        elif op == 6:
+            if get_value(i, 1) == 0:
+                i = get_value(i, 2)
+            else:
+                i+=3
+        elif op == 7:
+            set_value(i, 3, 1 if get_value(i, 1) < get_value(i, 2) else 0)
+            i+=4
+        elif op == 8:
+            set_value(i, 3, 1 if get_value(i, 1) == get_value(i, 2) else 0)
+            i+=4
         else:
             ValueError('Unknown opcode {}'.format(op))
 
 
 def part_one(program: List[int]):
     """ part one """
-    return computer(program)
+    computer(program)
 
 def part_two(program: List[int]):
     """ part two """
-    return 'todo'
+    computer(program, 5)
 
 def main():
     """ main """
@@ -65,9 +79,11 @@ def main():
         program = list(map(int, line.split(',')))
         
 
-    print('part_one', part_one(program.copy()))
+    print('\npart_one')
+    part_one(program.copy())
 
-    print('part_two', part_two(program.copy()))
+    print('\npart_two')
+    part_two(program.copy())
 
 
 main()
