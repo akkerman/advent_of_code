5d17b6e2435f06062be8c49e0d30667628132f04
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-11 09:18:54 +0100

    202412110918

--- 2024/11.py
+++ 2024/11.py
@@ -1,5 +1,8 @@
 """Day 11: Plutonian Pebbles."""
 import sys
+from functools import cache
+
+from utils import perf_timer
 
 def split_stone(stone: int) -> list[int]:
     """Split a stone."""
@@ -17,23 +20,41 @@ def blink(stones: list[int]) -> list[int]:
             new_stones.extend(split_stone(stone))
         else:
             new_stones.append(stone * 2024)
-
     return new_stones
 
 
-def part_one(stones: list[int], blinks: int = 25) -> int:
-    """Solution to part one."""
-    new_stones = stones
-    for _ in range(blinks):
-        new_stones = blink(new_stones)
-        # print(f'After {_ + 1} blinks:\n{new_stones}\n')
-    return len(new_stones)
+@perf_timer
+def part_one(stones: list[int]) -> int:
+    """Number of stones after 25 blinks."""
+    count = 0
+    for st in stones:
+        new_stones = [st]
+        for _ in range(25):
+            new_stones = blink(new_stones)
+        count+=len(new_stones)
+    return count
 
 
+@cache
+def solve(stone: int, blinks: int) -> int:
+    """Calculate the number of stones, from one stone after given blinks."""
+    if blinks == 0:
+        return 1
+    if len(str(stone)) % 2 == 0:
+        l, r = split_stone(stone)
+        return solve(l, blinks - 1) + solve(r, blinks - 1)
+    if stone == 0:
+        return solve(1, blinks - 1)
+    return solve(stone * 2024, blinks - 1)
+
+@perf_timer
 def part_two(stones: list[int]) -> int:
-    """Solution to part two."""
-    return part_one(stones, 75)
+    """Number of stones after 75 blinks."""
+    return sum(solve(stone, 75) for stone in stones)
 
+@perf_timer
+def test(stones: list[int]) -> int:
+    return sum(solve(stone, 25) for stone in stones)
 
 def main():
     """Parse input file, pass to puzzle solvers."""
@@ -41,10 +62,8 @@ def main():
     for line in sys.stdin:
         line = line.strip()
         stones.extend(list(map(int, line.split(' '))))
-        
 
     print('part_one', part_one(stones))
-
     print('part_two', part_two(stones))
 
 
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from typing import Callable, TypeVar, Any
 import re
 
 def find_occurences(pattern:str, line:str, overlap:bool=False):
@@ -12,3 +13,19 @@ def find_occurences(pattern:str, line:str, overlap:bool=False):
         indices.append(match.start())
 
     return indices
+
+T = TypeVar('T', bound=Callable[..., Any])
+def perf_timer(func: T) -> T:
+    """Decorator to time function performance."""
+    import time
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        duration = end - start
+        if duration < 1:
+            print(f'{func.__name__} took {duration * 1000:.2f} milliseconds')
+        else:
+            print(f'{func.__name__} took {duration} seconds')
+        return result
+    return wrapper
