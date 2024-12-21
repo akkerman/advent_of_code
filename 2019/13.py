337c8cd680455db44da92c7afe60a5ec30b395bd
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-21 21:31:40 +0100

    202412212131

--- 2019/13.py
+++ 2019/13.py
@@ -5,24 +5,70 @@ import re
 from collections import deque, defaultdict, Counter
 from functools import lru_cache
 from utils import perf_timer
+import os
+
+import time
 
 
 Coord = tuple[int, int]
 
+
+
+
+tiles = [' ', '#',  '█', '_', 'O']
+
+BALL = 4
+PADDLE = 3
+
+
 class Arcade:
     def __init__(self):
-        self.screen: dict[Coord, int] = defaultdict(int)
+        self.screen: dict[Coord, str] = defaultdict(lambda: ' ')
         self.params: list[int] = []
+        self.score = 0
+        self.frame = 0
+        self.ball = (0,0)
+        self.paddle = (0,0)
+        self.playing = False
 
     def draw(self, value:int):
         self.params.append(value)
         if len(self.params) == 3:
             x, y, tile = self.params
-            self.screen[(x,y)] = tile
+            if x == -1 and y == 0:
+                self.score = tile
+                self.params = []
+                return
+            if tile == BALL:
+                self.ball = (x,y)
+            if tile == PADDLE:
+                self.paddle = (x,y)
+            
+            self.screen[(x,y)] = tiles[tile]
             self.params = []
+            # if self.playing:
+            #     self.print_screen()
+            #     time.sleep(0.1)
 
-    def read(self):
-        return 1
+    def print_screen(self):
+        print("\033[H\033[J", end="")
+        self.frame+=1
+        print('Score:', self.score, 'Frame:', self.frame)
+        for y in range(0, 23):
+            for x in range(0, 40):
+                print(self.screen[(x,y)], end='')
+            print()
+        print()
+
+    def joystick(self):
+        self.playing = True
+        if self.ball[0] < self.paddle[0]:
+            return -1
+        elif self.ball[0] > self.paddle[0]:
+            return 1
+        else:
+            return 0
+        
 
 def computer(program: defaultdict[int,int], arcade: Arcade):
     """Computer."""
@@ -79,7 +125,7 @@ def computer(program: defaultdict[int,int], arcade: Arcade):
             write(i, 3, read(i, 1) * read(i, 2))
             i+=4
         elif op == INPUT:
-            write(i, 1, arcade.read())
+            write(i, 1, arcade.joystick())
             i+=2
         elif op == OUTPUT:
             arcade.draw(read(i, 1))
@@ -111,22 +157,25 @@ def part_one(program: list[int]) -> int:
     """Solution to part one."""
     arcade = Arcade()
 
+
     computer(defaultdict(int, enumerate(program)), arcade)
-    return Counter(arcade.screen.values())[2]
+    return Counter(arcade.screen.values())['█']
 
 
-def part_two(lines):
+def part_two(program: list[int]) -> int:
     """Solution to part two."""
-    return 'todo'
+    arcade = Arcade()
+
+    program[0] = 2
+    computer(defaultdict(int, enumerate(program)), arcade)
+    return arcade.score
 
 
 def main():
     """Parse input file, pass to puzzle solvers."""
     program: list[int] = []
     for line in fileinput.input():
-        line = line.strip()
-        program = list(map(int, line.split(',')))
-        break
+        program = list(map(int, line.strip().split(',')))
 
     print('part_one', part_one(program))
 
