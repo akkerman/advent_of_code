50c3f6080ce838672276d181914c3aabb02bdd01
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-22 10:41:42 +0100

    202412221041

--- 2024/22.py
+++ 2024/22.py
@@ -21,13 +21,6 @@ def next_secret(secret:int) -> int:
     new_secret = prune(mix(new_secret, new_secret * 2048))
     return new_secret
 
-# def next_secret(secret:int) -> int:
-#     """Generate the next secret number."""
-#     secret = (secret ^ (secret << 6)) % 20201227
-#     secret = (secret ^ (secret >> 5)) % 20201227
-#     secret = (secret ^ (secret << 11)) % 20201227
-#     return secret
-
 def next_secret_2000(secret:int) -> int:
     """Generate the 2000th secret number."""
     for _ in range(2000):
@@ -39,10 +32,48 @@ def part_one(secrets:list[int]) -> int:
     """Solution to part one."""
     return sum(next_secret_2000(secret) for secret in secrets)
 
+def last_digit(secret:int) -> int:
+    """Last digit of the secret."""
+    return int(str(secret)[-1])
 
-def part_two(secrets:list[int]) -> int:
+DiffSequence = tuple[int, int, int, int]
+LookupTable = dict[DiffSequence, int]
+
+def sequences(secret:int) -> LookupTable:
+    """Sequences of four differences and its corresponding price."""
+    diffs:list[int] = []
+    lookup: LookupTable = {}
+    prev = last_digit(secret)
+    for _ in range(2000):
+        secret = next_secret(secret)
+        last = last_digit(secret)
+        diffs.append(last - prev)
+        if len(diffs) >= 4:
+            seq = tuple(diffs[-4:])
+            if seq not in lookup:
+                lookup[seq] = last # type: ignore
+        prev = last
+    return lookup
+
+def part_two(secrets:list[int]) -> tuple[int, DiffSequence]:
     """Solution to part two."""
-    return -1
+    lookup_tables: list[LookupTable]  = [sequences(secret) for secret in secrets]
+    seqs = set(lookup_tables[0].keys()).union(*[lookup.keys() for lookup in lookup_tables])
+
+
+    max_bananas = 0
+    seq_for_monkey = (0,0,0,0)
+    for seq in seqs:
+        bananas = 0
+        for lookup in lookup_tables:
+            if seq in lookup:
+                bananas += lookup[seq]
+        if bananas > max_bananas:
+            max_bananas = bananas
+            seq_for_monkey = seq
+
+
+    return max_bananas, seq_for_monkey
 
 
 def main():
@@ -51,7 +82,6 @@ def main():
     for line in fileinput.input():
         line = int(line.strip())
         secrets.append(line)
-
 
     print('part_one', part_one(secrets))
 
@@ -73,10 +103,29 @@ def test_next_secret():
     assert next_secret(12249484) == 7753432 
     assert next_secret(7753432) == 5908254 
 
-
+def test_last_num():
+    assert last_digit(123) == 3
+    assert last_digit(15887950) == 0
+    assert last_digit(16495136) == 6
 
 def test_next_secret_2000():
     assert next_secret_2000(1) == 8685429
     assert next_secret_2000(10) == 4700978
     assert next_secret_2000(100) == 15273692
     assert next_secret_2000(2024) == 8667524
+
+def test_sequence():
+    seq = sequences(123)
+    assert seq[(-1, -1, 0, 2)] == 6
+
+def test_seq_example():
+    expected = (-2,1,-1,3)
+    assert expected in sequences(1)
+    assert expected in sequences(2)
+    assert expected not in sequences(3)
+    assert expected in sequences(2024)
+
+def test_part_two():
+    assert part_two([1,2,3,2024]) == (23, (-2,1,-1,3))
+
+
