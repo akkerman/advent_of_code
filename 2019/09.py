bb1b74c10d0e62a4b5a4db141ea1bc66639ea8a4
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-25 15:44:40 +0100

    202412251544
    Extract intcode computer

--- 2019/09.py
+++ 2019/09.py
@@ -1,108 +1,35 @@
 """Day 9: Sensor Boost."""
 import fileinput
-import heapq
-import re
-from collections import deque, defaultdict, Counter
-from functools import lru_cache
+from collections import defaultdict
+from intcode import computer, IO
+
+class SensorBoost(IO):
+    def __init__(self, value:int):
+        self.value = value
+        self.outputs = list[int]()
+
+    def input(self) -> int:
+        return self.value 
+
+    def output(self, value: int):
+        self.outputs.append(value)
 
 
-from utils import perf_timer
 
-def computer(program: defaultdict[int,int], input:int=1):
-    """Computer."""
-    ADD = 1
-    MULTIPLY = 2
-    INPUT = 3
-    OUTPUT = 4
-    JUMP_IF_TRUE = 5
-    JUMP_IF_FALSE = 6
-    LESS_THAN = 7
-    EQUALS = 8
-    ADJUST_RELATIVE_BASE = 9
-    HALT = 99
-
-    MODE_POSITION = 0
-    MODE_IMMEDIATE = 1
-    MODE_RELATIVE = 2
-
-    modes = [0,0,0,0]
-
-    output:list[int] = []
-
-    relative_base = 0
-
-    def write(instruction_pointer:int, parameter:int, value:int):
-        # parameters that an instruction writes to wil never be in immediate mode
-        if modes[parameter] == MODE_POSITION:
-            program[program[instruction_pointer+parameter]] = value
-        elif modes[parameter] == MODE_RELATIVE:
-            program[program[instruction_pointer+parameter] + relative_base] = value
-        else:
-            raise ValueError('Invalid mode for writing {}'.format(modes[parameter]))
-
-
-    def read(instruction_pointer:int, parameter:int):
-        if modes[parameter] == MODE_POSITION:
-            return program[program[instruction_pointer + parameter]]
-        elif modes[parameter] == MODE_IMMEDIATE:
-            return program[instruction_pointer + parameter]
-        elif modes[parameter] == MODE_RELATIVE:
-            return program[program[instruction_pointer + parameter] + relative_base]
-        else:
-            raise ValueError('Invalid mode for reading {}'.format(modes[parameter]))
-
-    i = 0
-    while i < len(program):
-        p3, p2, p1, o1, o2 = list(str(program[i]).zfill(5))
-        modes = [0, int(p1), int(p2), int(p3)]
-        op = int(o1 + o2)
-        if op == HALT:
-            break
-        elif op == ADD:
-            write(i, 3, read(i, 1) + read(i, 2))
-            i+=4
-        elif op == MULTIPLY:
-            write(i, 3, read(i, 1) * read(i, 2))
-            i+=4
-        elif op == INPUT:
-            write(i, 1, input)
-            i+=2
-        elif op == OUTPUT:
-            output.append(read(i, 1))
-            i+=2
-        elif op == JUMP_IF_TRUE:
-            if read(i, 1) != 0:
-                i = read(i, 2)
-            else:
-                i+=3
-        elif op == JUMP_IF_FALSE:
-            if read(i, 1) == 0:
-                i = read(i, 2)
-            else:
-                i+=3
-        elif op == LESS_THAN:
-            write(i, 3, 1 if read(i, 1) < read(i, 2) else 0)
-            i+=4
-        elif op == EQUALS:
-            write(i, 3, 1 if read(i, 1) == read(i, 2) else 0)
-            i+=4
-        elif op == ADJUST_RELATIVE_BASE:
-            relative_base += read(i, 1)
-            i+=2
-        else:
-            raise ValueError('Unknown opcode {}'.format(op))
-
-    return output
 
 def part_one(program:list[int]):
     """Produce the boost keycode."""
-    return computer(defaultdict(int, enumerate(program)))
+    sensorboost = SensorBoost(1)
+    computer(defaultdict(int, enumerate(program)), sensorboost)
+    return sensorboost.outputs[0]
 
 
 
 def part_two(program:list[int]):
     """Produce the coordinates of the distress signal."""
-    return computer(defaultdict(int, enumerate(program)), 2)
+    sensorboost = SensorBoost(2)
+    computer(defaultdict(int, enumerate(program)), sensorboost)
+    return sensorboost.outputs[0]
 
 
 def main():
--- 2019/13.py
+++ 2019/13.py
@@ -1,27 +1,16 @@
 """Day 13: Care Package."""
 import fileinput
-import heapq
-import re
-from collections import deque, defaultdict, Counter
-from functools import lru_cache
-from utils import perf_timer
-import os
-
-import time
-
+from collections import defaultdict, Counter
+from intcode import computer, IO
 
 Coord = tuple[int, int]
-
-
-
 
 tiles = [' ', '#',  '█', '_', 'O']
 
 BALL = 4
 PADDLE = 3
 
-
-class Arcade:
+class Arcade(IO):
     def __init__(self):
         self.screen: dict[Coord, str] = defaultdict(lambda: ' ')
         self.params: list[int] = []
@@ -30,6 +19,12 @@ class Arcade:
         self.ball = (0,0)
         self.paddle = (0,0)
         self.playing = False
+
+    def input(self):
+        return self.joystick()
+
+    def output(self, value:int):
+        self.draw(value)
 
     def draw(self, value:int):
         self.params.append(value)
@@ -69,89 +64,6 @@ class Arcade:
         else:
             return 0
         
-
-def computer(program: defaultdict[int,int], arcade: Arcade):
-    """Computer."""
-    ADD = 1
-    MULTIPLY = 2
-    INPUT = 3
-    OUTPUT = 4
-    JUMP_IF_TRUE = 5
-    JUMP_IF_FALSE = 6
-    LESS_THAN = 7
-    EQUALS = 8
-    ADJUST_RELATIVE_BASE = 9
-    HALT = 99
-
-    MODE_POSITION = 0
-    MODE_IMMEDIATE = 1
-    MODE_RELATIVE = 2
-
-    modes = [0,0,0,0]
-
-    relative_base = 0
-
-    def write(instruction_pointer:int, parameter:int, value:int):
-        # parameters that an instruction writes to wil never be in immediate mode
-        if modes[parameter] == MODE_POSITION:
-            program[program[instruction_pointer+parameter]] = value
-        elif modes[parameter] == MODE_RELATIVE:
-            program[program[instruction_pointer+parameter] + relative_base] = value
-        else:
-            raise ValueError('Invalid mode for writing {}'.format(modes[parameter]))
-
-
-    def read(instruction_pointer:int, parameter:int):
-        if modes[parameter] == MODE_POSITION:
-            return program[program[instruction_pointer + parameter]]
-        elif modes[parameter] == MODE_IMMEDIATE:
-            return program[instruction_pointer + parameter]
-        elif modes[parameter] == MODE_RELATIVE:
-            return program[program[instruction_pointer + parameter] + relative_base]
-        else:
-            raise ValueError('Invalid mode for reading {}'.format(modes[parameter]))
-
-    i = 0
-    while i < len(program):
-        p3, p2, p1, o1, o2 = list(str(program[i]).zfill(5))
-        modes = [0, int(p1), int(p2), int(p3)]
-        op = int(o1 + o2)
-        if op == HALT:
-            break
-        elif op == ADD:
-            write(i, 3, read(i, 1) + read(i, 2))
-            i+=4
-        elif op == MULTIPLY:
-            write(i, 3, read(i, 1) * read(i, 2))
-            i+=4
-        elif op == INPUT:
-            write(i, 1, arcade.joystick())
-            i+=2
-        elif op == OUTPUT:
-            arcade.draw(read(i, 1))
-            i+=2
-        elif op == JUMP_IF_TRUE:
-            if read(i, 1) != 0:
-                i = read(i, 2)
-            else:
-                i+=3
-        elif op == JUMP_IF_FALSE:
-            if read(i, 1) == 0:
-                i = read(i, 2)
-            else:
-                i+=3
-        elif op == LESS_THAN:
-            write(i, 3, 1 if read(i, 1) < read(i, 2) else 0)
-            i+=4
-        elif op == EQUALS:
-            write(i, 3, 1 if read(i, 1) == read(i, 2) else 0)
-            i+=4
-        elif op == ADJUST_RELATIVE_BASE:
-            relative_base += read(i, 1)
-            i+=2
-        else:
-            raise ValueError('Unknown opcode {}'.format(op))
-
 
 def part_one(program: list[int]) -> int:
     """Solution to part one."""
