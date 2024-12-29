1f68f6b5dc30bdfb41591a97107cde4a87942fbe
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-29 07:53:46 +0100

    202412290753

--- 2019/10.py
+++ 2019/10.py
@@ -1,5 +1,6 @@
 """Day 10: Monitoring Station."""
 import fileinput
+from collections import defaultdict
 import math
 
 def part_one_vector(asteroids: list[tuple[int,int]]):
@@ -51,11 +52,41 @@ def part_one_angle(asteroids: list[tuple[int,int]]):
 
     return max_asteroids, best_asteroid
 
+def manhattan_distance(a: tuple[int,int], b: tuple[int,int]) -> int:
+    """Return the Manhattan distance between two points."""
+    x1, y1 = a
+    x2, y2 = b
+    return abs(x2 - x1) + abs(y2 - y1)
+
+def normalize_angle(angle: float) -> float:
+    """Normalize an angle to be between 0 and 2pi."""
+    normalized = (angle - 3* math.pi / 2) % (2 * math.pi)
+    return normalized
 
 
-def part_two(lines):
+
+def part_two(asteroids: list[tuple[int,int]], station: tuple[int,int] = (11,13)):
     """Solution to part two."""
-    return 'todo'
+    angles = defaultdict[float, list[tuple[int,int]]](lambda: [])
+
+    for asteroid in asteroids:
+        if station == asteroid:
+            continue
+
+        xs, ys = station
+        xa, ya = asteroid
+        angle = math.atan2(ya - ys, xa - xs)
+        angles[angle].append(asteroid)
+
+    for k,v in angles.items():
+        angles[k] = sorted(v, key=lambda x: manhattan_distance(station, x))
+
+    sorted_angles = sorted(angles.keys(), key=normalize_angle)
+    
+    vaporized_asteroid = angles[sorted_angles[199]][0]
+    x, y = vaporized_asteroid
+
+    return x * 100 + y, vaporized_asteroid
 
 
 def main():
@@ -69,10 +100,13 @@ def main():
                 asteroids.append((x, y))
         y += 1
 
-    print('part_one', part_one_vector(asteroids))
-    print('part_one', part_one_angle(asteroids))
+    print('part_one', *part_one_vector(asteroids))
 
-    print('part_two', part_two(asteroids))
+    nums, station = part_one_angle(asteroids)
+    print('part_one', nums, station)
+
+    # too low: 1808
+    print('part_two', *part_two(asteroids, station))
 
 
 if __name__ == '__main__':
