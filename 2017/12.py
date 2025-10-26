d981c4bd56a4601d07f275271ddc4bfa26f7278f
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-10-26 12:19:51 +0100

    202510261219

--- 2017/12.py
+++ 2017/12.py
@@ -1,43 +1,57 @@
 """Day 12: Digital Plumber."""
 import fileinput
-import heapq
-import re
-from collections import deque, defaultdict, Counter
-from functools import lru_cache
+from collections import deque, defaultdict
 from typing import Dict
-from utils import perf_timer
 
-def part_one(door_dict: Dict[str, list[int]]):
+def part_one(door_dict: Dict[int, list[int]]):
     """Solution to part one."""
+
+    return len(determine_group(door_dict))
+
+def determine_group(door_dict: Dict[int, list[int]], start: int=0) -> set[int]:
     group:set[int] = set()
-    group.add(0)
+    group.add(start)
     queue:deque[int] = deque()
-    queue.append(0)
+    queue.append(start)
     while queue:
         current = queue.popleft()
-        for door in door_dict[str(current)]:
+        for door in door_dict[current]:
             if door not in group:
                 group.add(door)
                 queue.append(door)
 
-    return len(group)
+    return group
 
 
-def part_two(lines):
+def part_two(door_dict: Dict[int, list[int]]):
     """Solution to part two."""
-    return 'todo'
+    groups: list[set[int]] = []
+    
+    all_doors = set(door_dict.keys())
+
+    current_door = 0
+
+    while True:
+        groups.append(determine_group(door_dict, current_door))
+        
+        remaining_doors = all_doors - {elem for s in groups for elem in s}
+
+        if not remaining_doors:
+            return len(groups)
+
+        current_door = min(remaining_doors)
 
 
 def main():
     """Parse input file, pass to puzzle solvers."""
-    door_dict: Dict[str, list[int]] = defaultdict(list[int])
+    door_dict: Dict[int, list[int]] = defaultdict(list[int])
     for line in fileinput.input():
         line = line.strip()
         fd, doors =  line.split(' <-> ')
         doors = list(map(int, doors.split(', ')))
         
         
-        door_dict[fd].extend(doors)
+        door_dict[int(fd)].extend(doors)
 
     print('part_one', part_one(door_dict))
 
