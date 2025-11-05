e5256f9d5be7d8022fab9f024e5a0229cb830252
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-11-05 18:12:25 +0100

    202511051812

--- 2016/22.py
+++ 2016/22.py
@@ -24,11 +24,86 @@ def part_one(disk_usages: list[DiskUsage]):
             1 for a, b in permutations(disk_usages, 2)
             if a.used != 0 and a != b and a.used <= b.avail
             )
+
+
+Coord = tuple[int, int]
+TOP_COORD = re.compile(r'/dev/grid/node-x(\d+)-y0')
+NODE_COORD = re.compile(r'/dev/grid/node-x(\d+)-y(\d+)')
+
+
+
+def print_grid(disk_usages: list[DiskUsage]):
+    """Print the grid."""
+    nodes: dict[Coord, DiskUsage] = {}
+    max_x = 0
+    max_y = 0
+    for du in disk_usages:
+        if (res := NODE_COORD.match(du.fs)):
+            x = int(res.group(1))
+            y = int(res.group(2))
+            nodes[(x, y)] = du
+            max_x = max(max_x, x)
+            max_y = max(max_y, y)
+
+    def neighbors(c: Coord) -> list[Coord]:
+        """Get adjacent coordinates."""
+        x, y = c
+        nbs: list[Coord] = []
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = x + dx, y + dy
+            nbs.append((nx, ny))
+        return nbs
+
+    immovable: set[Coord] = set()
+    for coord, du in nodes.items():
+        if du.used == 0:
+            continue  # empty node kan altijd data ontvangen
+        if not any(
+            du.used <= nodes[n].size
+            for n in neighbors(coord)
+            if n in nodes
+        ):
+            immovable.add(coord)
+
+    max_available = max(
+        du.avail
+        for _, du in nodes.items()
+    )
+    print('max available', max_available)
+
+    for y in range(max_y + 1):
+        row = ''
+        for x in range(max_x + 1):
+            du = nodes[(x, y)]
+            if du.used == 0:
+                row += ' _ '
+            elif du.used > max_available:
+                row += ' # '
+            else:
+                row += ' . '
+        print(row)
+
+def top_right_coord(disk_usages: list[DiskUsage]) -> Coord:
+    """Find the top right coordinate."""
+    max_x = max(
+        int(res.group(1))
+        for du in disk_usages
+        if (res := TOP_COORD.match(du.fs))
+        )
+    return (max_x, 0)
+
+def part_two(disk_usages: list[DiskUsage]):
+    """Solution to part two."""
+
+
+    gain_access = top_right_coord(disk_usages)
+    print('goal data at', gain_access)
+
+    print_grid(disk_usages)
+
+
     
 
-
-def part_two(lines):
-    """Solution to part two."""
     return 'todo'
 
 
