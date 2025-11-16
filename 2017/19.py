73eacb228f7c8bf47a92cc7784ac195604e295b2
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-11-16 07:47:00 +0100

    202511160747

--- 2017/19.py
+++ 2017/19.py
@@ -1,10 +1,5 @@
 """2017 Day 19: A Series of Tubes"""
 import fileinput
-import heapq
-import re
-from collections import deque, defaultdict, Counter
-from functools import lru_cache
-from utils import perf_timer
 import string
 
 Coord = tuple[int, int]
@@ -17,17 +12,17 @@ def add(p: Coord, q: Coord):
     qx,qy = q
     return (px+qx,py+qy)
 
-def part_one(diagram: dict[Coord, str], start: Coord):
-    """Solution to part one."""
-
+def solve(diagram: dict[Coord, str], start: Coord):
     letters: list[str] = []
     pos = start
     direction = (0, 1)  # initially going down 
+    steps = 0
 
     while True:
         if pos not in diagram:
-            return "".join(letters)
+            return "".join(letters), steps
         char = diagram[pos]
+        steps+=1
 
         if char == '+':
             change = horz_dir if direction in vert_dir else vert_dir
@@ -36,29 +31,12 @@ def part_one(diagram: dict[Coord, str], start: Coord):
                 if nb in diagram:
                     pos = nb
                     direction = change[i]
-
             continue
 
         if char in string.ascii_uppercase:
             letters.append(char)
             
         pos = add(pos,direction)
-
-
-
-def part_two(diagram: dict[Coord, str]):
-    """Solution to part two."""
-    return 'todo'
-
-def print_diagram(diagram: dict[Coord,str]):
-    for y in range(8):
-        for x in range(18):
-            pos = (x,y)
-            if pos in diagram:
-                print(diagram[pos], end='')
-            else:
-                print(' ', end='')
-        print('')
 
 def main():
     """Parse input file, pass to puzzle solvers."""
@@ -74,9 +52,12 @@ def main():
                 # there is only one entry point on the top row
                 start = (x, y)
       
-    print('part_one', part_one(diagram, start))
 
-    print('part_two', part_two(diagram))
+    letters, steps = solve(diagram,start)
+
+    print('part_one', letters)
+
+    print('part_two', steps)
 
 
 if __name__ == '__main__':
