cb051d532a25439f239edc775c39f206f878e1ea
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-12-02 07:31:26 +0100

    202512020731

--- 2025/02.py
+++ 2025/02.py
@@ -2,47 +2,35 @@
 import fileinput
 import re
 from utils import chunk_list
+from typing import Callable
 
-def is_invalid_id(id: int) -> bool:
+def is_invalid_id_part1(id: int) -> bool:
+    """ Check if the given id is invalid. It is invalid if a pattern is repeated twice."""
     id_str = str(id)
     mid = len(id_str) // 2
     return id_str[:mid] == id_str[mid:]
 
-def part_one(ranges:list[tuple[int,int]]):
-    """Solution to part one."""
-    total = 0
-    for x,y in ranges:
-        for id in range(x, y+1):
-            if is_invalid_id(id):
-                total += id
-    return total
-
-def is_invalid_id2(id: int) -> bool:
+def is_invalid_id_part2(id: int) -> bool:
+    """ Check if the given id is invalid. It is invalid if it is a repeating pattern."""
     id_str = str(id)
-    for i in range(1, len(id_str)):
+    mid = len(id_str) // 2 
+    for i in range(1, mid + 1):
         parts: list[list[str]] = chunk_list(id_str, i)
         if all(part == parts[0] for part in parts):
             return True
     return False
 
-def part_two(ranges:list[tuple[int,int]]):
-    """Solution to part two."""
-    total = 0
-    for x,y in ranges:
-        for id in range(x, y+1):
-            if is_invalid_id2(id):
-                total += id
-    return total
-
+def solve(ranges:list[tuple[int,int]], is_invalid_fn: Callable[[int],bool]) -> int:
+    """Sum all invalid IDs in the given ranges."""
+    return sum(id for x,y in ranges for id in range(x, y+1) if is_invalid_fn(id))
 
 def main():
     """Parse input file, pass to puzzle solvers."""
     line: str = next(fileinput.input()).strip()
     ranges: list[tuple[int,int]] = [(int(x),int(y)) for x,y in re.findall(r'(\d+)-(\d+)', line)]
 
-    print('part_one', part_one(ranges))
-
-    print('part_two', part_two(ranges))
+    print('part_one', solve(ranges, is_invalid_id_part1))
+    print('part_two', solve(ranges, is_invalid_id_part2))
 
 
 if __name__ == '__main__':
