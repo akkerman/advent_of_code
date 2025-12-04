3ff6ce324ed36c3bd89f507922c0e900803da1ab
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-12-04 08:36:21 +0100

    202512040836

--- 2025/04.py
+++ 2025/04.py
@@ -1,48 +1,33 @@
 """2025 Day 4: Printing Department"""
 import fileinput
-import heapq
-import re
-from collections import deque, defaultdict, Counter
-from functools import lru_cache
-from utils import perf_timer
 
 Coord = tuple[int, int]
-Grid = list[Coord]
+Grid = set[Coord]
 
-def neighbors(c:Coord):
+def neighbors(c:Coord) -> Grid:
     """Return all 8 neighboring coordinates."""
     x,y = c
-    for dx in [-1,0,1]:
-        for dy in [-1,0,1]:
-            if dx == 0 and dy == 0:
-                continue
-            yield (x+dx, y+dy)
+    return {(x+dx, y+dy) 
+            for dx in (-1,0,1) for dy in (-1,0,1) 
+            if dx or dy}
 
-def mark_movable(grid:Grid):
-    movable = set[Coord]()
-    for x,y in grid:
-        if sum(1 for c in neighbors((x,y)) if c in grid) < 4:
-            movable.add((x,y))
-    return movable
+def mark_movable(grid:Grid) -> Grid:
+    """Return set of movable coordinates."""
+    return {
+      c for c in grid
+      if len(neighbors(c) & grid) < 4
+    }
 
-def part_one(grid:Grid):
+def part_one(grid:Grid) -> int:
     """Solution to part one."""
     return len(mark_movable(grid))
-            
 
-
-def part_two(grid:Grid):
+def part_two(grid:Grid) -> int:
     """Solution to part two."""
     current = set[Coord](grid)
-
-    while True:
-        movable = mark_movable(current)
-        if not movable:
-            return len(grid) - len(current)
-
-        current = current - movable
-
-
+    while movable := mark_movable(current):
+        current -= movable
+    return len(grid - current)
 
 def main():
     """Parse input file, pass to puzzle solvers."""
@@ -51,13 +36,11 @@ def main():
     for y,line in enumerate(fileinput.input()):
         for x,c in enumerate(line.strip()):
             if c == '@':
-                grid.append((x,y))
+                grid.add((x,y))
 
 
     print('part_one', part_one(grid))
-
     print('part_two', part_two(grid))
-
 
 if __name__ == '__main__':
     main()
