83f0fe73315795b41b97e81e3f7b6dcc2a3e24cd
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-12-04 07:06:13 +0100

    202512040706

--- 2025/04.py
+++ 2025/04.py
@@ -18,19 +18,30 @@ def neighbors(c:Coord):
                 continue
             yield (x+dx, y+dy)
 
-def part_one(grid:Grid):
-    """Solution to part one."""
-    count = 0
+def mark_movable(grid:Grid):
+    movable = set[Coord]()
     for x,y in grid:
         if sum(1 for c in neighbors((x,y)) if c in grid) < 4:
-            count += 1
-    return count
+            movable.add((x,y))
+    return movable
+
+def part_one(grid:Grid):
+    """Solution to part one."""
+    return len(mark_movable(grid))
             
 
 
-def part_two(lines):
+def part_two(grid:Grid):
     """Solution to part two."""
-    return 'todo'
+    current = set[Coord](grid)
+
+    while True:
+        movable = mark_movable(current)
+        if not movable:
+            return len(grid) - len(current)
+
+        current = current - movable
+
 
 
 def main():
