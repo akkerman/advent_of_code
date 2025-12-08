3ee0665b113250a55866e5ebff78d886e5473abc
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-12-08 21:40:20 +0100

    202512082140

--- 2025/08.py
+++ 2025/08.py
@@ -1,136 +1,74 @@
 """2025 Day 8: Playground"""
 import fileinput
-import heapq
-import re
-from collections import deque, defaultdict, Counter
-from functools import lru_cache
-from utils import perf_timer
-
 import math
+from utils import perf_timer
+from itertools import combinations
 
 Coord = tuple[int, int, int]
+JunctionBoxId = int
+Pair = tuple[JunctionBoxId, JunctionBoxId]
+Circuit = set[JunctionBoxId]
 
 def euclidean_distance(a: Coord, b: Coord) -> float:
     """Calculate Euclidean distance between two 3D coordinates."""
     return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2) ** 0.5
 
+@perf_timer
+def pairs_sorted_by_distance(locs: list[Coord]) -> list[Pair]:
+    """Generate all unique pairs of junction box locations, sorted by distance."""
+    distances = dict[Pair, float]()
+    for i in range(len(locs)):
+        for j in range(i + 1, len(locs)):
+            dist = euclidean_distance(locs[i], locs[j])
+            distances[(i, j)] = dist
+
+    return sorted(distances, key=lambda k: distances[k])
+
+def create_circuits(pairs:list[Pair], num_junction_boxes: int = 0) -> tuple[list[Circuit], Pair]:
+    """Create circuits from all pairs or until all junction boxes are connected."""
+    circuits = dict[JunctionBoxId, Circuit]()
+    left, right = -1, -1
+    for left, right in pairs:
+        if left not in circuits and right not in circuits:
+            circuits[left] = set[int]([left,right])
+            circuits[right] = circuits[left]
+
+        elif left in circuits and right in circuits:
+            if circuits[left] is not circuits[right]:
+                union = circuits[left] | circuits[right]
+                for member in union:
+                    circuits[member] = union
+            # else they are already connected
+
+        elif right in circuits:
+            circuits[right].add(left)
+            circuits[left] = circuits[right]
+
+        else: 
+            assert left in circuits
+            circuits[left].add(right)
+            circuits[right] = circuits[left]
+
+        if len(circuits[left]) == num_junction_boxes:
+            assert len(circuits[right]) == num_junction_boxes
+            break
+
+    return list(circuits.values()), (left, right)
+
 def part_one(locations: list[Coord]):
     """Solution to part one."""
-    distances = dict[tuple[int, int], float]()
-    for i, coord in enumerate(locations):
-        for j in range(i + 1, len(locations)):
-            other = locations[j]
-            idx = (i, j) if i < j else (j, i)
-            dist = euclidean_distance(coord, other)
-            distances[idx] = dist
-
-    sorted_distances = sorted(distances.items(), key=lambda x: x[1])
-
-    connected = dict[int, set[int]]()
-    max = 10 if len(locations) < 1000 else 1000
-
-    for idx in range(max):
-        sd = sorted_distances[idx]
-        (i, j), dist = sd
-
-        if i not in connected and j not in connected:
-            pair = set[int]([i,j])
-            connected[i] = pair
-            connected[j] = pair
-            continue
-
-        if i in connected and j in connected:
-            # both are connect to other nodes
-            if connected[i] is connected[j]:
-                continue
-            else: # merge sets
-                assert i not in connected[j] and j not in connected[i]
-
-                union = connected[i] | connected[j]
-
-                for member in union:
-                    connected[member] = union
-
-                continue
-        if j in connected:
-            connected[j].add(i)
-            connected[i] = connected[j]
-            continue
-        if i in connected:
-            connected[i].add(j)
-            connected[j] = connected[i]
-            continue
-
-        assert False, "Should not reach here"
-     
-    
-    # sorted values by length
-    uniq = {tuple(conn) for conn in connected.values()}
-    
-
-    groups = sorted(uniq, key=lambda x: len(x), reverse=True)
-    first_three = groups[:3]
-    return math.prod(len(g) for g in first_three)
-
-
+    pairs = pairs_sorted_by_distance(locations)
+    max_pairs = 10 if len(locations) < 1000 else 1000
+    circuits, _ = create_circuits(pairs[:max_pairs], num_junction_boxes = len(locations))
+    unique_circuits = {frozenset(s) for s in circuits}
+    sorted_circuits = sorted(unique_circuits, key=lambda x: len(x), reverse=True)
+    return math.prod(len(g) for g in sorted_circuits[:3])
 
 def part_two(locations: list[Coord]):
     """Solution to part two."""
-    distances = dict[tuple[int, int], float]()
-    for i, coord in enumerate(locations):
-        for j in range(i + 1, len(locations)):
-            other = locations[j]
-            idx = (i, j) if i < j else (j, i)
-            dist = euclidean_distance(coord, other)
-            distances[idx] = dist
-
-    sorted_distances = sorted(distances.items(), key=lambda x: x[1])
-
-    connected = dict[int, set[int]]()
-
-    for (i, j), dist in sorted_distances:
-
-        if i not in connected and j not in connected:
-            pair = set[int]([i,j])
-            connected[i] = pair
-            connected[j] = pair
-            continue
-
-        if i in connected and j in connected:
-            # both are connect to other nodes
-            if connected[i] is connected[j]:
-                continue
-            else: # merge sets
-                assert i not in connected[j] and j not in connected[i]
-
-                union = connected[i] | connected[j]
-
-                if len(union) == len(locations):
-                    return locations[i][0] * locations[j][0]
-
-                for member in union:
-                    connected[member] = union
-
-                continue
-        if j in connected:
-            connected[j].add(i)
-            connected[i] = connected[j]
-            if len(connected[j]) == len(locations):
-                return locations[i][0] * locations[j][0]
-            continue
-        if i in connected:
-            connected[i].add(j)
-            connected[j] = connected[i]
-            if len(connected[i]) == len(locations):
-                return locations[i][0] * locations[j][0]
-            continue
-
-        assert False, "Should not reach end of loop"
-
-
-    print(connected)
-    assert False, "Should not reach end of function"
-     
+    pairs = pairs_sorted_by_distance(locations)
+    _, (left, right) = create_circuits(pairs, num_junction_boxes = len(locations))
+    return locations[left][0] * locations[right][0]
 
 
 def main():
@@ -143,9 +81,7 @@ def main():
         locations.append(coord)
 
 
-   # too low 576
     print('part_one', part_one(locations))
-
     print('part_two', part_two(locations))
 
 
